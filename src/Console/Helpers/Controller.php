470b2fabcaf141e647a6ce5460f33b46d73b4ae8
Chad Chapman <chad.chapman2010@gmail.com>
2025-03-30 21:29:20 -0400

    Added space after void for onConstruct

--- src/Console/Helpers/Controller.php
+++ src/Console/Helpers/Controller.php
@@ -27,7 +27,7 @@ class '.$controllerName.'Controller extends Controller {
      *
      * @return void
      */
-    public function onConstruct(): void{
+    public function onConstruct(): void {
         $this->view->setLayout(\''.$layout.'\');
     }
 }
@@ -55,7 +55,7 @@ class '.$controllerName.'Controller extends Controller {
      *
      * @return void
      */
-    public function onConstruct(): void{
+    public function onConstruct(): void {
         $this->view->setLayout(\''.$layout.'\');
     }
 
