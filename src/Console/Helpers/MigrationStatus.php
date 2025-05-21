416d8d6162a4857ea319019cd2cf9608971607b8
Chad Chapman <chad.chapman2010@gmail.com>
2025-05-20 20:30:45 -0400

    #21 - Implemented the class

--- src/Console/Helpers/MigrationStatus.php
+++ src/Console/Helpers/MigrationStatus.php
@@ -7,9 +7,45 @@ class MigrationStatus {
     private $name;
     private $status;
 
-    public function __construct(string $batch, string $name, string $status = '') {
+    /**
+     * Setups the MigrationStatus object
+     *
+     * @param string $batch The batch value for a particular migration.
+     * @param string $name The name of the migration class.
+     * @param bool $isRan Default value is false.  It represents whether or 
+     * not a migration has been ran.
+     */
+    public function __construct(string $batch, string $name, bool $isRan = false) {
         $this->batch = $batch;
         $this->name = $name;
-        $this->status = $status;
+        $this->status = ($isRan) ? 'Ran' : 'Pending';
+    }
+
+    /**
+     * Getter function for batch;
+     *
+     * @return string $batch The value for batch.
+     */
+    public function getBatch(): string {
+        return $this->batch;
+    }
+
+    /**
+     * Getter function for migration class' name.
+     *
+     * @return string $name The name of the migration class.
+     */
+    public function getName(): string {
+        return $this->name;
+    }
+
+    /**
+     * Getter function for status of the migration.
+     *
+     * @return string $status The status of the migration represented as a 
+     * string.
+     */
+    public function getStatus(): string {
+        return $this->status;
     }
 }
