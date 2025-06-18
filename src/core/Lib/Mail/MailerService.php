97b751c1fec7331b0df64613f7e4383335e8ee96
Chad Chapman <chad.chapman2010@gmail.com>
2025-06-18 18:24:25 -0400

    #29 - Basic implementation of mailer

--- src/core/Lib/Mail/MailerService.php
+++ src/core/Lib/Mail/MailerService.php
@@ -5,7 +5,33 @@ use Symfony\Component\Mailer\Transport;
 use Symfony\Component\Mailer\Mailer;
 use Symfony\Component\Mime\Email;
 use Core\Lib\Utilities\Env;
+use Throwable;
 
 class MailerService {
-    
+    protected Mailer $mailer;
+
+    /**
+     * Creates a new mailer.
+     */
+    public function __construct() {
+        $dsn = Env::get('MAILER_DSN');
+        $transport = Transport::fromDsn($dsn);
+        $this->mailer = new Mailer($transport);
+    }
+
+    public function send(string $to, string $subject, string $htmlBody): bool {
+        try {
+            $email = (new Email())
+                ->from(Env::get('MAIL_FROM_ADDRESS'))
+                ->to($to)
+                ->subject($subject)
+                ->html($htmlBody);
+
+            $this->mailer->send($email);
+            return true;
+        } catch (Throwable $e) {
+            logger($e, 'error');
+            return false;
+        }
+    }
 }
