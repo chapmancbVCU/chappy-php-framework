7c409f09d01c3357a7120065bdd47c993f44c414
Chad Chapman <chad.chapman2010@gmail.com>
2025-06-18 18:16:52 -0400

    #29 - Initial commit

---
+++ src/core/Lib/Mail/MailerService.php
@@ -0,0 +1,11 @@
+<?php
+namespace Core\Lib\Mail;
+
+use Symfony\Component\Mailer\Transport;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mime\Email;
+use Core\Lib\Utilities\Env;
+
+class MailerService {
+    
+}
