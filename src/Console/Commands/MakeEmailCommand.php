3939550308d7631812725ea840e080e48309c950
Chad Chapman <chad.chapman2010@gmail.com>
2025-06-19 11:11:11 -0400

    #29 - Removed unused imports

--- src/Console/Commands/MakeEmailCommand.php
+++ src/Console/Commands/MakeEmailCommand.php
@@ -1,11 +1,8 @@
 <?php
 namespace Console\Commands;
- 
-use Console\Helpers\View;
-use Console\Helpers\Tools;
+
 use Console\Helpers\Email;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
