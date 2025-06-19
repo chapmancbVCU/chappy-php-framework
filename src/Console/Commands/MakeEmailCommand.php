f9271cf8fa64e499da4d5f0f66de4f41c7571163
Chad Chapman <chad.chapman2010@gmail.com>
2025-06-18 20:44:02 -0400

    #29 - Added make email command

---
+++ src/Console/Commands/MakeEmailCommand.php
@@ -0,0 +1,40 @@
+<?php
+namespace Console\Commands;
+ 
+use Console\Helpers\View;
+use Console\Helpers\Tools;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Generates a new email.
+ */
+class MakeEmailCommand extends Command {
+    /**
+     * Configures the command.
+     *
+     * @return void
+     */
+    protected function configure(): void
+    {
+        $this->setName('make:email')
+            ->setDescription('Generates a new email')
+            ->setHelp('php console make:email <email_name>')
+            ->addArgument('email-name', InputArgument::REQUIRED, 'Pass the name of the new email');
+    }
+
+    /**
+     * Executes the command
+     *
+     * @param InputInterface $input The input.
+     * @param OutputInterface $output The output.
+     * @return int A value that indicates success, invalid, or failure.
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        return Command::SUCCESS;
+    }
+}
