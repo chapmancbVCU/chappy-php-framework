2b806355ca0d093b7e6c65f447215a7f205aef40
Chad Chapman <chad.chapman2010@gmail.com>
2025-06-18 20:56:47 -0400

    29 - Added function stubs

--- src/Console/Helpers/Email.php
+++ src/Console/Helpers/Email.php
@@ -1,6 +1,26 @@
 <?php
 
+use Symfony\Component\Console\Command\Command;
+
 class Email {
     protected static string $layoutPath = CHAPPY_BASE_PATH.DS.'resources'.DS.'views'.DS.'emails'.DS.'layouts'.DS;
     protected static string $templatePath = CHAPPY_BASE_PATH.DS.'resources'.DS.'views'.DS.'emails'.DS;
+
+    public static function emailTemplate(): string {
+        return '';
+    }
+
+    public static function layoutTemplate(): string {
+        return '';
+    }
+
+    public static function makeEmail(): int {
+
+        return Command::SUCCESS;
+    }
+
+    public static function makeLayout(): int {
+
+        return Command::SUCCESS;
+    }
 }
