7dc36a6ed622184642dd06bf47698f9d33892dbb
Chad Chapman <chad.chapman2010@gmail.com>
2025-06-19 10:41:39 -0400

    #29 - Added function that tests if paths for emails and email layouts exists

--- src/Console/Helpers/Email.php
+++ src/Console/Helpers/Email.php
@@ -8,14 +8,25 @@ class Email {
     protected static string $layoutPath = CHAPPY_BASE_PATH.DS.'resources'.DS.'views'.DS.'emails'.DS.'layouts'.DS;
     protected static string $templatePath = CHAPPY_BASE_PATH.DS.'resources'.DS.'views'.DS.'emails'.DS;
 
+    public static function emailPath(): void {
+        if(!is_dir(self::$templatePath)) {
+            mkdir(self::$templatePath, 0755, true);
+        }
+    }
+
+    public static function layoutPath(): void {
+        if(!is_dir(self::$layoutPath)) {
+            mkdir(self::$layoutPath, 0755, true);
+        }
+    }
 
     public static function layoutTemplate(): string {
         return '';
     }
 
     public static function makeEmail(InputInterface $input): int {
+        self::emailPath();
         $emailName = self::$templatePath . $input->getArgument('email-name') . '.php';
-        dd($emailName);
         return Tools::writeFile($emailName, '', 'E-mail file');
     }
 
