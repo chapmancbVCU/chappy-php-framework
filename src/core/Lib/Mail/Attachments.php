1e729a29922ad4feb45188a62017fc9b237d0b03
Chad Chapman <chad.chapman2010@gmail.com>
2025-06-21 15:54:17 -0400

    #29 - Moved functions for processing attachments to its own class

---
+++ src/core/Lib/Mail/Attachments.php
@@ -0,0 +1,93 @@
+<?php
+declare(strict_types=1);
+namespace Core\Lib\Mail;
+
+use Core\Lib\Utilities\Arr;
+use Symfony\Component\Mime\Email;
+
+class Attachments {
+    const MIME_7ZIP = 'application/x-7z-compressed';
+    const MIME_BMP = 'image/bmp';
+    const MIME_CSS = 'text/css';
+    const MIME_CSV = 'text/csv';
+    const MIME_DOC = 'application/msword';
+    const MIME_DOCX = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
+    const MIME_GIF = 'image/gif';
+    const MIME_GZIP = 'application/gzip';
+    const MIME_JAVASCRIPT = 'application/javascript';
+    const MIME_JPG = 'image/jpeg';
+    const MIME_JSON = 'application/json';
+    const MIME_MARKDOWN = 'text/markdown';
+    const MIME_HTML = 'text/html';
+    const MIME_PDF = 'application/pdf';
+    const MIME_PHP = 'application/x-httpd-php';
+    const MIME_PNG = 'image/png';
+    const MIME_SVG = 'image/svg+xml';
+    const MIME_TAR = 'application/x-tar';
+    const MIME_TEXT = 'text/plain';
+    const MIME_WEBP = 'image/webp';
+    const MIME_XLS = 'application/vnd.ms-excel';
+    const MIME_XLSX = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+    const MIME_XML = 'application/xml';
+    const MIME_ZIP = 'application/zip';
+
+    /**
+     * Processes attachment if key labeled content is found.
+     *
+     * @param array $attachment The attachment.
+     * @param Email $email The Email to be sent.
+     * @return Email $email The Email to be sent after attachments have been 
+     * processed.
+     */
+    protected static function attach(array $attachment, Email $email): Email {
+        return $email->attach(
+            $attachment['content'],
+            $attachment['name'] ?? null,
+            $attachment['mime'] ?? null
+        );
+    }
+
+    /**
+     * Processes attachment if key labeled path is found.
+     *
+     * @param array $attachment The attachment.
+     * @param Email $email The Email to be sent.
+     * @return Email $email The Email to be sent after attachments have been 
+     * processed.
+     */
+    protected static function attachFromPath(array $attachment, Email $email): Email {
+        return $email->attachFromPath(
+            $attachment['path'],
+            $attachment['name'] ?? null,
+            $attachment['mime'] ?? null
+        );
+    }
+
+    /**
+     * Process attachments to be sent.
+     *
+     * @param array $attachments The array of attachments.
+     * @param Email $email The Email to be sent.
+     * @return Email $email The Email to be sent after attachments have been 
+     * processed.
+     */
+    public static function processAttachments(array $attachments, Email $email): Email {
+        if(Arr::isAssoc($attachments)) {
+            if(isset($attachments['path'])) {
+                $email = self::attachFromPath($attachments, $email);
+            } else if(isset($attachments['content'])) {
+                $email = self::attach($attachments, $email);
+            }
+            return $email;
+        }
+
+        foreach($attachments as $attachment) {
+            if(isset($attachment['path'])) {
+                $email = self::attachFromPath($attachment, $email);
+            } else if(isset($attachment['content'])) {
+                $email = self::attach($attachment, $email);
+            }
+        }
+        return $email;
+    }
+}
--- src/core/Lib/Mail/MailerService.php
+++ src/core/Lib/Mail/MailerService.php
@@ -14,30 +14,6 @@ use Symfony\Component\Mailer\Transport;
 class MailerService {
     protected static string $layoutPath = CHAPPY_BASE_PATH.DS.'resources'.DS.'views'.DS.'emails'.DS.'layouts'.DS;
     protected Mailer $mailer;
-    const MIME_7ZIP = 'application/x-7z-compressed';
-    const MIME_BMP = 'image/bmp';
-    const MIME_CSS = 'text/css';
-    const MIME_CSV = 'text/csv';
-    const MIME_DOC = 'application/msword';
-    const MIME_DOCX = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
-    const MIME_GIF = 'image/gif';
-    const MIME_GZIP = 'application/gzip';
-    const MIME_JAVASCRIPT = 'application/javascript';
-    const MIME_JPG = 'image/jpeg';
-    const MIME_JSON = 'application/json';
-    const MIME_MARKDOWN = 'text/markdown';
-    const MIME_HTML = 'text/html';
-    const MIME_PDF = 'application/pdf';
-    const MIME_PHP = 'application/x-httpd-php';
-    const MIME_PNG = 'image/png';
-    const MIME_SVG = 'image/svg+xml';
-    const MIME_TAR = 'application/x-tar';
-    const MIME_TEXT = 'text/plain';
-    const MIME_WEBP = 'image/webp';
-    const MIME_XLS = 'application/vnd.ms-excel';
-    const MIME_XLSX = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
-    const MIME_XML = 'application/xml';
-    const MIME_ZIP = 'application/zip';
     protected static string $templatePath = CHAPPY_BASE_PATH.DS.'resources'.DS.'views'.DS.'emails'.DS;
     
     /**
@@ -47,38 +23,6 @@ class MailerService {
         $dsn = Env::get('MAILER_DSN');
         $transport = Transport::fromDsn($dsn);
         $this->mailer = new Mailer($transport);
-    }
-
-    /**
-     * Processes attachment if key labeled content is found.
-     *
-     * @param array $attachment The attachment.
-     * @param Email $email The Email to be sent.
-     * @return Email $email The Email to be sent after attachments have been 
-     * processed.
-     */
-    protected static function attach(array $attachment, Email $email): Email {
-        return $email->attach(
-            $attachment['content'],
-            $attachment['name'] ?? null,
-            $attachment['mime'] ?? null
-        );
-    }
-
-    /**
-     * Processes attachment if key labeled path is found.
-     *
-     * @param array $attachment The attachment.
-     * @param Email $email The Email to be sent.
-     * @return Email $email The Email to be sent after attachments have been 
-     * processed.
-     */
-    protected static function attachFromPath(array $attachment, Email $email): Email {
-        return $email->attachFromPath(
-            $attachment['path'],
-            $attachment['name'] ?? null,
-            $attachment['mime'] ?? null
-        );
     }
 
     /**
@@ -127,34 +71,6 @@ class MailerService {
     }
 
     /**
-     * Process attachments to be sent.
-     *
-     * @param array $attachments The array of attachments.
-     * @param Email $email The Email to be sent.
-     * @return Email $email The Email to be sent after attachments have been 
-     * processed.
-     */
-    protected static function processAttachments(array $attachments, Email $email): Email {
-        if(Arr::isAssoc($attachments)) {
-            if(isset($attachments['path'])) {
-                $email = self::attachFromPath($attachments, $email);
-            } else if(isset($attachments['content'])) {
-                $email = self::attach($attachments, $email);
-            }
-            return $email;
-        }
-
-        foreach($attachments as $attachment) {
-            if(isset($attachment['path'])) {
-                $email = self::attachFromPath($attachment, $email);
-            } else if(isset($attachment['content'])) {
-                $email = self::attach($attachment, $email);
-            }
-        }
-        return $email;
-    }
-
-    /**
      * Renders template file.
      *
      * @param string $path Path to the template.
@@ -186,7 +102,7 @@ class MailerService {
                 ->html($htmlBody);
 
             if(!Arr::isEmpty($attachments)) {
-                $email = self::processAttachments($attachments, $email);
+                $email = Attachments::processAttachments($attachments, $email);
             }
 
             $this->mailer->send($email);
@@ -236,7 +152,7 @@ class MailerService {
                 ->html($htmlBody);
 
             if(!Arr::isEmpty($attachments)) {
-                $email = self::processAttachments($attachments, $email);
+                $email = Attachments::processAttachments($attachments, $email);
             }
 
             $this->mailer->send($email);
