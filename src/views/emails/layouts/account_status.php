996811a4a64199db178a38ee8232f0f9a7494169
Chad Chapman <chad.chapman2010@gmail.com>
2025-06-22 14:31:28 -0400

    #29 - Renamed file

--- src/views/emails/layouts/account_status.php
+++ src/views/emails/layouts/account_status.php
@@ -5,12 +5,15 @@
     <style>
         body { font-family: sans-serif; }
         .email-wrapper { padding: 20px; border: 1px solid #eee; }
+        .center { text-align: center; }
     </style>
 </head>
 <body>
     <div class="email-wrapper">
-        <h1>Template</h1>
+        <h1 class="center">Account Status</h1>
         <?= $content ?>
+        <hr>
+        <p>This E-mail was auto generated and may not be monitored.  If you are not the intended recipient contact the site administrator and delete this E-mail.</p>
     </div>
 </body>
 </html>
