bb3abbc12c5af791129771e31e7fbd0cd0ab1742
Chad Chapman <chad.chapman2010@gmail.com>
2025-06-22 20:09:29 -0400

    #29 - Fixed route parameters for controller and action

--- src/views/emails/reset_password.php
+++ src/views/emails/reset_password.php
@@ -1,5 +1,5 @@
 <h1>Reset password for <?=e($user->username)?></h1>
 <p>
     An event occurred that requires you to reset your password.  
-    You can reset your password <a href="<?=route('profile.updatePassword', [$user->id])?>">here</a>
+    You can reset your password <a href="<?=route('auth.resetPassword', [$user->id])?>">here</a>
 </p>
