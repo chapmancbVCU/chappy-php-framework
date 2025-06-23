193a2bb97402f658733f81177ea6a61d169293ac
Chad Chapman <chad.chapman2010@gmail.com>
2025-06-22 20:52:12 -0400

    #29 - Initial commit

---
+++ src/core/Lib/Mail/UpdatePasswordMailer.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+namespace Core\Lib\Mail;
+
+use Core\Lib\Mail\MailerService;
+use App\Models\Users;
+
+/**
+ * Class for generating a message informing the user that their 
+ * password has been updated.
+ */
+class UpdatePasswordMailer {
+    /**
+     * Generates and sends E-mail informing the user that their 
+     * password has been updated.
+     *
+     * @param Users $user The new user.
+     * @return bool True if sent, otherwise false.
+     */
+    public static function send(Users $user): bool {
+        $mail = new MailerService();
+        $subject = 'The password update notification for ' . $user->username;
+
+        return $mail->sendTemplate(
+            'chad.chapman2010+welcome_test@gmail.com',
+            $subject,
+            'update_password',
+            ['user' => $user],
+            'default',
+            [],
+            MailerService::FRAMEWORK_LAYOUT_PATH,
+            MailerService::FRAMEWORK_TEMPLATE_PATH,
+            'default',
+            MailerService::FRAMEWORK_STYLES_PATH
+        );
+    }
+}
---
+++ src/views/emails/update_password.php
@@ -0,0 +1,5 @@
+<h1>Notice: The password for <?=e($user->username)?> has been updated!</h1>
+<p>
+    Contact your administrator if you did not update your password.  Otherwise, 
+    this message can be ignored.
+</p>
