29d14fc4606fb8f96f00fd6b102e66bb4a03497e
Chad Chapman <chad.chapman2010@gmail.com>
2025-06-23 21:01:18 -0400

    #29 - Fixed path for attachments

--- src/core/Lib/Mail/Attachments.php
+++ src/core/Lib/Mail/Attachments.php
@@ -9,7 +9,8 @@ use Symfony\Component\Mime\Email;
  * Supports attachment processing for MailerService.
  */
 class Attachments {
-    public const ATTACHMENTS_PATH = 'storage'.DS.'private'.DS.'email_attachments'.DS; 
+    public const ATTACHMENTS_PATH = CHAPPY_BASE_PATH.DS.'storage'.DS.'app'.DS.'private'.DS.'email_attachments'.DS; 
+    
     public const MIME_TYPES = [
         '7z'   => 'application/x-7z-compressed',
         'ai'   => 'application/postscript',
