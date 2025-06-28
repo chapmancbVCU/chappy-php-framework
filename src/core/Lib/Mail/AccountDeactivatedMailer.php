048e477f789b446144a0d346810b4eb98f6b08f6
Chad Chapman <chad.chapman2010@gmail.com>
2025-06-28 17:50:25 -0400

    #29 - Removed unused import

--- src/core/Lib/Mail/AccountDeactivatedMailer.php
+++ src/core/Lib/Mail/AccountDeactivatedMailer.php
@@ -2,14 +2,20 @@
 declare(strict_types=1);
 namespace Core\Lib\Mail;
 
-use Core\Lib\Mail\MailerService;
 use App\Models\Users;
 
 /**
  * Class for generating a message informing the user that their 
  * password has been updated.
  */
-class AccountDeactivatedMailer {
+class AccountDeactivatedMailer extends AbstractMailer {
+    protected Users $user;
+
+    public function __construct(Users $user) {
+        parent::__construct();
+        $this->user = $user;
+    }
+
     /**
      * Generates and sends E-mail informing the user that their 
      * password has been updated.
@@ -17,21 +23,18 @@ class AccountDeactivatedMailer {
      * @param Users $user The new user.
      * @return bool True if sent, otherwise false.
      */
-    public static function send(Users $user): bool {
-        $mail = new MailerService();
+    public function send(): bool {
         $subject = 'Notice: Your account has been deactivated';
 
-        return $mail->sendTemplate(
-            $user->email,
+        return $this->buildAndSend(
+            $this->user->email,
             $subject,
             'deactivated_account',
-            ['user' => $user],
-            'default',
-            [],
-            MailerService::FRAMEWORK_LAYOUT_PATH,
-            MailerService::FRAMEWORK_TEMPLATE_PATH,
-            'default',
-            MailerService::FRAMEWORK_STYLES_PATH
+            ['user' => $this->user],
         );
+    }
+
+    public static function sendTo(Users $user): bool {
+        return (new static($user))->send();
     }
 }
