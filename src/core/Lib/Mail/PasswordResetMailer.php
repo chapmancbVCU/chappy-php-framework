561d3c6d14fc9f3ee868b21af32456b0274a6871
Chad Chapman <chad.chapman2010@gmail.com>
2025-06-28 17:56:37 -0400

    #29 - Refactored to extend AbstractMailer class

--- src/core/Lib/Mail/PasswordResetMailer.php
+++ src/core/Lib/Mail/PasswordResetMailer.php
@@ -2,34 +2,37 @@
 declare(strict_types=1);
 namespace Core\Lib\Mail;
 
-use Core\Lib\Mail\MailerService;
 use App\Models\Users;
 
 /**
  * Class for generating a reset password E-mail.
  */
-class PasswordResetMailer {
+class PasswordResetMailer extends AbstractMailer {
+    protected Users $user;
+
+    public function __construct(Users $user) {
+        parent::__construct();
+        $this->user = $user;
+    }
+
     /**
      * Generates and sends reset password E-mail.
      *
      * @param Users $user The user whose password needs to be reset.
      * @return bool True if sent, otherwise false.
      */
-    public static function send(Users $user): bool {
-        $mail = new MailerService();
-        $subject = $user->username . ', please reset your password';
+    public function send(): bool {
+        $subject = $this->user->username . ', please reset your password';
 
-        return $mail->sendTemplate(
-            $user->email,
+        return $this->buildAndSend(
+            $this->user->email,
             $subject,
             'reset_password',
-            ['user' => $user],
-            'default',
-            [],
-            MailerService::FRAMEWORK_LAYOUT_PATH,
-            MailerService::FRAMEWORK_TEMPLATE_PATH,
-            'default',
-            MailerService::FRAMEWORK_STYLES_PATH
+            ['user' => $this->user],
         );
+    }
+
+    public static function sendTo(Users $user): bool {
+        return (new static($user))->send();
     }
 }
--- src/core/Lib/Mail/UpdatePasswordMailer.php
+++ src/core/Lib/Mail/UpdatePasswordMailer.php
@@ -2,14 +2,20 @@
 declare(strict_types=1);
 namespace Core\Lib\Mail;
 
-use Core\Lib\Mail\MailerService;
 use App\Models\Users;
 
 /**
  * Class for generating a message informing the user that their 
  * password has been updated.
  */
-class UpdatePasswordMailer {
+class UpdatePasswordMailer extends AbstractMailer {
+    protected Users $user;
+
+    public function __construct(Users $user) {
+        parent::__construct();
+        $this->user = $user;
+    }
+
     /**
      * Generates and sends E-mail informing the user that their 
      * password has been updated.
@@ -17,21 +23,18 @@ class UpdatePasswordMailer {
      * @param Users $user The new user.
      * @return bool True if sent, otherwise false.
      */
-    public static function send(Users $user): bool {
-        $mail = new MailerService();
-        $subject = 'The password update notification for ' . $user->username;
+    public function send(): bool {
+        $subject = 'The password update notification for ' . $this->user->username;
 
-        return $mail->sendTemplate(
-            $user->email,
+        return $this->buildAndSend(
+            $this->user->email,
             $subject,
             'update_password',
-            ['user' => $user],
-            'default',
-            [],
-            MailerService::FRAMEWORK_LAYOUT_PATH,
-            MailerService::FRAMEWORK_TEMPLATE_PATH,
-            'default',
-            MailerService::FRAMEWORK_STYLES_PATH
+            ['user' => $this->user],
         );
+    }
+
+    public static function sendTo(Users $user): bool {
+        return (new static($user))->send();
     }
 }
