14f4ab567a6d2dfb2152ecb6edb851ddb1853789
Chad Chapman <chad.chapman2010@gmail.com>
2025-06-28 20:18:46 -0400

    #29 - Initial commit

---
+++ src/Console/Commands/MakeMailerCommand.php
@@ -0,0 +1,38 @@
+<?php
+namespace Console\Commands;
+
+use Console\Helpers\Email;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Generates a new custom mailer.
+ */
+class MakeMailerCommand extends Command {
+    /**
+     * Configures the command.
+     *
+     * @return void
+     */
+    protected function configure(): void
+    {
+        $this->setName('make:mailer')
+            ->setDescription('Generates a new custom mailer')
+            ->setHelp('php console make:mailer <email_name>')
+            ->addArgument('mailer-name', InputArgument::REQUIRED, 'Pass the name of the new custom mailer');
+    }
+
+    /**
+     * Executes the command
+     *
+     * @param InputInterface $input The input.
+     * @param OutputInterface $output The output.
+     * @return int A value that indicates success, invalid, or failure.
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        return Email::makeEmail($input);
+    }
+}
