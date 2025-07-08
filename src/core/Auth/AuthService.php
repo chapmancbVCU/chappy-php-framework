98c6b28539b56351935313df838b54f1d2cb1bda
Chad Chapman <chad.chapman2010@gmail.com>
2025-07-07 21:33:47 -0400

    #31 - Added logout function

--- src/core/Auth/AuthService.php
+++ src/core/Auth/AuthService.php
@@ -3,9 +3,18 @@ namespace core\Auth;
 use Core\Input;
 use App\Models\Users;
 use Core\Models\Login;
+use Core\Lib\Utilities\Env;
 use Core\Lib\Logging\Logger;
 
 class AuthService {
+    /**
+     * Processes login attempts
+     *
+     * @param Input $request The request for the login
+     * @param Login $loginModel The login model
+     * @param string $username The user to be logged in
+     * @return Login Model that handles logins.
+     */
     public static function attemptLogin(Input $request, Login $loginModel, string $username) : Login {
         $user = Users::findByUsername($username);
         if($user && password_verify($request->get('password'), $user->password)) {
@@ -23,7 +32,7 @@ class AuthService {
             redirect('home');
         }  else {
             if($user) {
-                $loginModel = Users::loginAttempts($user, $loginModel);
+                $loginModel = self::loginAttempts($user, $loginModel);
             }
             else {
                 $loginModel->addErrorMessage('username','There is an error with your username or password');
@@ -32,5 +41,36 @@ class AuthService {
         }
 
         return $loginModel;
+    }
+
+    /**
+     * Tests for login attempts and sets session messages when there is a 
+     * failed attempt or when account is locked.
+     *
+     * @param User $user The user whose login attempts we are tracking.
+     * @param Login $loginModel The model that will be responsible for 
+     * displaying messages.
+     * @return Login $loginModel The Login model after login in attempt test 
+     * and session messages are assigned.
+     */
+    public static function loginAttempts($user, $loginModel) {
+        if($user->login_attempts >= Env::get('MAX_LOGIN_ATTEMPTS', 5)) {
+            $user->inactive = 1; 
+        }
+        if($user->login_attempts < Env::get('MAX_LOGIN_ATTEMPTS', 5)) {
+            $loginModel->addErrorMessage('username', 'There is an error with your username or password.');
+        } else {
+            flashMessage('danger', 'Your account has been locked due to too many failed login attempts.');
+        }
+        $user->login_attempts = $user->login_attempts + 1;
+        $user->save();
+        return $loginModel;
+    }
+
+    public static function logout() {
+        $user = Users::currentUser();
+        if($user) {
+            $user->logout();
+        }
     }
 }
