1175e3fb5815bf5f7e1d81248f8cca06e021ec5f
Chad Chapman <chad.chapman2010@gmail.com>
2025-07-07 21:39:56 -0400

    #31 - Moved setAclAtRegistration to core

--- src/core/Auth/AuthService.php
+++ src/core/Auth/AuthService.php
@@ -78,4 +78,18 @@ class AuthService {
             $user->logout();
         }
     }
+
+    /**
+     * Sets ACL at registration.  If users table is empty the default 
+     * value is Admin.  Otherwise, we set the value to "".
+     *
+     * @return string The value of the ACL we are setting upon 
+     * registration of a user.
+     */
+    public static function setAclAtRegistration() {
+        if(Users::findTotal() == 0) {
+            return '["Admin"]';
+        }
+        return '[""]';
+    }
 }
