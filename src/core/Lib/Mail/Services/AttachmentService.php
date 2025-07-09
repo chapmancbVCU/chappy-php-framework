4748324fd03dcf89dccbfe1888542277c5343437
Chad Chapman <chad.chapman2010@gmail.com>
2025-07-08 21:00:59 -0400

    #31 - Added function to create upload object for attachment

--- src/core/Lib/Mail/Services/AttachmentService.php
+++ src/core/Lib/Mail/Services/AttachmentService.php
@@ -1,10 +1,25 @@
 <?php
 namespace Core\Lib\Mail\Services;
 
+use Core\Lib\FileSystem\Uploads;
 use Core\Models\EmailAttachments;
 
 class AttachmentService {
 
+    public static function attachmentUpload(EmailAttachments $attachment): ?Uploads {
+        if($attachment->isNew()) {
+            $uploads = Uploads::handleUpload(
+                $_FILES['attachment_name'],
+                EmailAttachments::class,
+                ROOT.DS,
+                '15mb',
+                $attachment,
+                'attachment_name'
+            );
+            return $uploads;
+        }
+        return null;
+    }
 
     public static function deleteAttachment(EmailAttachments $attachment): void {
         if($attachment && file_exists($attachment->path)) {
@@ -12,4 +27,6 @@ class AttachmentService {
             $attachment->delete();
         }
     }
+
+    
 }
