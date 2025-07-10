386e3bba7baf9fe0debf172f27d9d66534eafc51
Chad Chapman <chad.chapman2010@gmail.com>
2025-07-09 20:15:11 -0400

    #31 - Declare strict typing

--- src/core/Auth/AuthService.php
+++ src/core/Auth/AuthService.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 namespace core\Auth;
 use Core\DB;
 use Core\Input;
--- src/core/Lib/Mail/Services/AttachmentService.php
+++ src/core/Lib/Mail/Services/AttachmentService.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 namespace Core\Lib\Mail\Services;
 
 use Core\Lib\Mail\Attachments;
