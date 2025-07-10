bef6d011a9c5fad0105c62e1ca2a69cf7be7d145
Chad Chapman <chad.chapman2010@gmail.com>
2025-07-09 21:41:15 -0400

    #31 - Created paginateUsers function to return paginated list of users

--- src/core/Administration/DashboardService.php
+++ src/core/Administration/DashboardService.php
@@ -4,6 +4,7 @@ namespace Core\Administration;
 
 use core\Auth\AuthService;
 use App\Models\Users;
+use Core\Lib\Pagination\Pagination;
 
 final class DashboardService {
 
@@ -12,5 +13,13 @@ final class DashboardService {
             'conditions' => 'id != ?',
             'bind' => [AuthService::currentUser()->id]
         ]);
+    }
+
+    public static function paginateUsers(Pagination $pagination) {
+        return Users::find($pagination->paginationParams(
+            'id != ?',
+            [AuthService::currentUser()->id],
+            'created_at DESC'
+        ));
     }
 }
