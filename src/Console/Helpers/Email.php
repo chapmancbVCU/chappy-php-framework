92b7e846c29c8fcf65f4dfc623893d87c4454d53
Chad Chapman <chad.chapman2010@gmail.com>
2025-07-09 20:19:01 -0400

    Declare strict typing

--- src/Console/Helpers/Email.php
+++ src/Console/Helpers/Email.php
@@ -1,8 +1,8 @@
 <?php
+declare(strict_types=1);
 namespace Console\Helpers;
 use Console\Helpers\Tools;
 use Core\Lib\Utilities\Str;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 
 
