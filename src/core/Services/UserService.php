2598ca315c5bc2c6d84ae1dcdcf7da0d03bbb93d
Chad Chapman <chad.chapman2010@gmail.com>
2025-07-11 19:28:50 -0400

    #31 - Added deleteIfAllowed function

--- src/core/Services/UserService.php
+++ src/core/Services/UserService.php
@@ -3,8 +3,18 @@ declare(strict_types=1);
 namespace core\Services;
 
 use App\Models\Users;
+use Core\Models\ProfileImages;
 
 final class UserService {
 
-    
+    public static function deleteIfAllowed(int $id, bool $unlink) {
+        $user = Users::findById((int)$id);
+        if($user && $user->acl != '["Admin"]') {
+            ProfileImages::deleteImages($id, $unlink);
+            $user->delete();
+            flashMessage('success', 'User has been deleted.');
+        } else {
+            flashMessage('danger', 'Cannot delete Admin user!');
+        }
+    }
 }
