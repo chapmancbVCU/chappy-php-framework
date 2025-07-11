b4226a2b4186f0aafe64a7731293abeb776082bf
Chad Chapman <chad.chapman2010@gmail.com>
2025-07-11 19:40:44 -0400

    #31 - Moved status and reset password functions to UserService

--- src/core/Services/DashboardService.php
+++ src/core/Services/DashboardService.php
@@ -23,26 +23,6 @@ final class DashboardService {
     }
 
     /**
-     * Assist in toggling inactive field.
-     *
-     * @param Input $request The request.
-     * @return integer 1 if inactive is 'on', otherwise we return 0.
-     */
-    public static function toggleAccountStatus(Input $request) {
-        return ($request->get('inactive') == 'on') ? 1 : 0;
-    }
-
-    /**
-     * Assist in toggling reset_password field.
-     *
-     * @param Input $request The request.
-     * @return integer 1 if reset_password is 'on', otherwise we return 0.
-     */
-    public static function toggleResetPassword(Input $request): int {
-        return ($request->get('reset_password') == 'on') ? 1 : 0;
-    }
-
-    /**
      * Returns query number of users excluding current.
      *
      * @return int The number of users except current.
