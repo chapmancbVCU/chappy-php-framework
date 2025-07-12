ad63e6f25a55f09781f8064e87a73411cc743da1
Chad Chapman <chad.chapman2010@gmail.com>
2025-07-12 19:38:39 -0400

    #31 - Created function to make services directory if it does not exist and added variable for path

--- src/Console/Helpers/Services.php
+++ src/Console/Helpers/Services.php
@@ -3,5 +3,16 @@ declare(strict_types=1);
 namespace Console\Helpers;
 
 class Services {
-    
+    protected static string $servicesPath = CHAPPY_BASE_PATH.DS.'app'.DS.'Services'.DS;
+
+    /**
+     * Creates directory for services if it does not exist.
+     *
+     * @return void
+     */
+    public static function servicesPath(): void {
+        if(!is_dir(self::$servicesPath)) {
+            mkdir(self::$servicesPath, 0755, true);
+        }
+    }
 }
