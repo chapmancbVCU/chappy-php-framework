50213eff161f0582edbf35351fb933dfc70aba53
Chad Chapman <chad.chapman2010@gmail.com>
2025-07-12 16:05:36 -0400

    #31 - Debugged issue where email was not send and added to phpdoc info about  flag

--- src/core/Services/UserService.php
+++ src/core/Services/UserService.php
@@ -104,6 +104,8 @@ final class UserService {
      * Assist in toggling inactive field.
      *
      * @param Input $request The request.
+     * @param bool $mailer Sends account deactivated E-mail when user 
+     * surpasses max number of login attempts before account is locked.
      * @return int 1 if inactive is 'on', otherwise we return 0.
      */
     public static function toggleAccountStatus(Users $user, Input $request, bool $mailer = false) {
@@ -111,7 +113,7 @@ final class UserService {
         $user->inactive = ($request->get('inactive') == 'on') ? 1 : 0;
         $user->login_attempts = ($user->inactive == 0) ? 0 : $user->login_attempts;
 
-        if($previousInactiveState == 0 && $user->inactive == 1 && $mailer == true) {
+        if($previousInactiveState === 'on' && $user->inactive == 1 && $mailer == true) {
             AccountDeactivatedMailer::sendTo($user);
         }
     }
