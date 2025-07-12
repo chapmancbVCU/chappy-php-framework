729ac185ec60e5b69b5fc2f1074af7b00fd32617
Chad Chapman <chad.chapman2010@gmail.com>
2025-07-12 19:32:49 -0400

    #31 - Initial commit

---
+++ src/Console/Commands/MakeServiceCommand.php
@@ -0,0 +1,37 @@
+<?php
+namespace Console\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Generates a new custom mailer.
+ */
+class MakeServiceCommand extends Command {
+    /**
+     * Configures the command.
+     *
+     * @return void
+     */
+    protected function configure(): void
+    {
+        $this->setName('make:service')
+            ->setDescription('Generates a new service class')
+            ->setHelp('php console make:mailer <service_name>')
+            ->addArgument('mailer-name', InputArgument::REQUIRED, 'Pass the name of the new service');
+    }
+
+    /**
+     * Executes the command
+     *
+     * @param InputInterface $input The input.
+     * @param OutputInterface $output The output.
+     * @return int A value that indicates success, invalid, or failure.
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+
+    }
+}
