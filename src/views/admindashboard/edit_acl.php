9b73817d314b78b73246a273998b64b65d308f5e
Chad Chapman <chad.chapman2010@gmail.com>
2025-07-13 13:27:26 -0400

    #31 - Initial commit

---
+++ src/views/admindashboard/edit_acl.php
@@ -0,0 +1,18 @@
+<?php $this->setSiteTitle("Add ACL"); ?>
+<?php $this->start('body'); ?>
+<h1 class="text-center">Add ACL</h1>
+
+<div class="row align-items-center justify-content-center">
+    <div class="col-md-6 bg-light p-3">
+        <form class="form" action="" method="post">
+            <?= csrf() ?>
+            <?= input('text', "ACL", 'acl', $this->acl->acl, ['class' => 'form-control input-sm'], ['class' => 'form-group'], $this->displayErrors) ?>
+            <div class="col-md-12 text-end pt-3">
+                <a href="<?=route('admindashboard.manageACLs')?>" class="btn btn-default">Cancel</a>
+                <?= submit('Save',['class'=>'btn btn-primary']) ?>
+            </div>
+        </form>
+    </div>
+</div>
+
+<?php $this->end(); ?>
