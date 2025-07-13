ad9775e3d4a48e8c36a3b77d7add92254d5b9ab2
Chad Chapman <chad.chapman2010@gmail.com>
2025-07-13 14:23:45 -0400

    #33 - Make directory if not exits

--- src/Console/Helpers/CommandHelper.php
+++ src/Console/Helpers/CommandHelper.php
@@ -67,7 +67,8 @@ class '.$commandName.'Command extends Command {
      */
     public static function makeCommand(InputInterface $input): int {
         $commandName = $input->getArgument('command-name');
-        $fullPath = self::COMMAND_PATH.$commandName.'Command.php';
+        Tools::pathExists(self::COMMAND_PATH);
+        $fullPath = self::COMMAND_PATH.$commandName.DS.'Command';
         return Tools::writeFile($fullPath, self::commandTemplate($commandName), 'Command');
     }
 
