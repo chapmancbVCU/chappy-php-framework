cbb03c8e8503462682fd79ce2f86b8dd61732ed2
Chad Chapman <chad.chapman2010@gmail.com>
2025-07-15 20:26:00 -0400

    #36 - Updated description for error

--- src/views/restricted/not_found.php
+++ src/views/restricted/not_found.php
@@ -1,5 +1,5 @@
 <?php $this->setSiteTitle("404 - Not found"); ?>
 <?php $this->start('body'); ?>
 <h1 class="text-center">404 - Not found</h1>
-<h2 class="text-center red">The view '<?=$this->actionName?>' does not exist in the controller '<?= $this->controllerName ?>'.</h2>
+<h2 class="text-center red">The action '<?=$this->actionName?>' does not exist in the controller '<?= $this->controllerName ?>'.</h2>
 <?php $this->end(); ?>
