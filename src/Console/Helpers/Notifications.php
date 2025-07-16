8d61f113fe63a70fcd41faef7e193a2739b1d284
Chad Chapman <chad.chapman2010@gmail.com>
2025-07-16 19:49:54 -0400

    #37 - Initial commit with template and function to create migration

---
+++ src/Console/Helpers/Notifications.php
@@ -0,0 +1,71 @@
+<?php
+declare(strict_types=1);
+namespace Console\Helpers;
+
+/**
+ * Supports commands related to notifications.
+ */
+class Notifications {
+    /**
+     * Template for notifications migration.
+     *
+     * @param string $fileName The file an class name.
+     * @return string The contents for the notifications migration.
+     */
+    public static function migrationTemplate(string $fileName): string {
+        return '<?php
+namespace Database\Migrations;
+use Core\Lib\Database\Schema;
+use Core\Lib\Database\Blueprint;
+use Core\Lib\Database\Migration;
+
+/**
+ * Migration class for the notifications table.
+ */
+class '.$fileName.' extends Migration {
+    /**
+     * Performs a migration for a new table.
+     *
+     * @return void
+     */
+    public function up(): void {
+        Schema::create(\'notifications\', function (Blueprint $table) {
+            $table->uuid(\'id\');
+            $table->primary(\'id\');
+            $table->string(\'type\');
+            $table->string(\'notifiable_type\');
+            $table->unsignedBigInteger(\'notifiable_id\');
+            $table->text(\'data\');
+            $table->timestamp(\'read_at\')->nullable();
+            $table->timestamps();
+            $table->index(\'notifiable_type\');
+            $table->index(\'notifiable_id\');
+        });
+    }
+
+    /**
+     * Undo a migration task.
+     *
+     * @return void
+     */
+    public function down(): void {
+        Schema::dropIfExists(\'notifications\');
+    }
+}
+';
+    }
+
+    /**
+     * Creates new notifications migration.
+     *
+     * @return int A value that indicates success, invalid, or failure.
+     */
+    public static function notificationsMigration(): int {
+        $fileName = Migrate::fileName();
+        return Tools::writeFile(
+            Migrate::MIGRATIONS_PATH.$fileName.'.php',
+            self::migrationTemplate($fileName),
+            'Notifications migration'
+        );
+    }
+}
