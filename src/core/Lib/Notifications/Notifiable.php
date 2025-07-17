04ac0dffd4aa105dba285a9e2a5aae3710b55341
Chad Chapman <chad.chapman2010@gmail.com>
2025-07-17 18:21:26 -0400

    #37 - Initial commit

---
+++ src/core/Lib/Notifications/Notifiable.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+namespace Core\Lib\Notifications;
+
+use Core\Models\Notifications;
+use Ramsey\Uuid\Uuid;
+
+trait Notifiable {
+    public function notify(Notification $notification): void {
+        foreach($notification->via($this) as $channel) {
+            if($channel == 'database') {
+                $data = $notification->toDatabase($this);
+                $record = new Notifications();
+                $record->id = Uuid::uuid4()->toString();
+                $record->type = get_class($notification);
+                $record->notifiable_type = get_class($this);
+                $record->notifiable_id = $this->id;
+                $record->data = json_encode($data);
+                $record->read_at = null;
+                $record->save();
+            }
+        }
+    }
+
+    public function notifications(): array {
+        return Notifications::findAllByUserId($this->id);
+    }
+}
