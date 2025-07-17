ca15fa44a2a7f212349a9b4feeb2d26c4a5a0c70
Chad Chapman <chad.chapman2010@gmail.com>
2025-07-17 18:35:47 -0400

    #37 - Initial commit

---
+++ src/core/Lib/Notifications/UserRegistered.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Notifications;
+
+use Core\Lib\Notifications\Notification;
+
+class UserRegistered extends Notification
+{
+    protected $user;
+
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Specify which channels to deliver to.
+     */
+    public function via($notifiable): array
+    {
+        return ['database']; // we only use database for now
+    }
+
+    /**
+     * Data stored in the notifications table.
+     */
+    public function toDatabase($notifiable): array
+    {
+        return [
+            'user_id'   => $this->user->id,
+            'username'  => $this->user->username ?? $this->user->email,
+            'message'   => "A new user has registered: {$this->user->username}",
+            'registered_at' => date('Y-m-d H:i:s')
+        ];
+    }
+}
