6c270dabe27ef8663cdddfab6e12957a742e3530
Chad Chapman <chad.chapman2010@gmail.com>
2025-07-17 21:28:52 -0400

    #37 - Added function to display notifications

--- src/core/Services/NotificationService.php
+++ src/core/Services/NotificationService.php
@@ -3,11 +3,29 @@ declare(strict_types=1);
 namespace Core\Services;
 
 use App\Models\Users;
+use Core\Models\Notifications;
 use Core\Lib\Notifications\UserRegistered;
 
 class NotificationService {
 
-    public static function sendUserRegistrationNotification(Users $newUser) {
+    public static function flashUnreadNotifications() {
+        $admin = AuthService::currentUser();
+        $messages = [];
+        foreach ($admin->notifications() as $notification) {
+            $data = json_decode($notification->data, true);
+            if (isset($data['message'])) {
+                $messages[] = $data['message'];
+            }
+            Notifications::markAsReadById($notification->id);
+        }
+        
+        if (!empty($messages)) {
+            $finalMessage = implode('<br>', $messages);
+            flashMessage('info', $finalMessage);
+        }
+    }
+
+    public static function sendUserRegistrationNotification(Users $newUser): void {
         $admins = Users::find([
             'conditions' => 'deleted = ?',
             'bind' => [0]
