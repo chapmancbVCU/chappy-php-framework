023a158d6564c0ed78fd8e3d08ef61c5ccf6ad5f
Chad Chapman <chad.chapman2010@gmail.com>
2025-07-18 20:14:24 -0400

    #38 - Implemented EventDispatcher

--- src/core/Lib/Events/EventDispatcher.php
+++ src/core/Lib/Events/EventDispatcher.php
@@ -3,5 +3,32 @@ declare(strict_types=1);
 namespace Core\Lib\Events;
 
 class EventDispatcher {
-    
+    protected array $listeners = [];
+
+    /**
+     * Listens for an event.
+     *
+     * @param string $eventName The event name.
+     * @param callable $listener The listener.
+     * @return void
+     */
+    public function listen(string $eventName, callable $listener): void {
+        $this->listeners[$eventName][] = $listener;
+    }
+
+    /**
+     * Dispatches an event
+     *
+     * @param object $event The event to be dispatches.
+     * @return void
+     */
+    public function dispatch(object $event): void {
+        $eventName = get_class($event);
+
+        if(!empty($this->listeners[$eventName])) {
+            foreach($this->listeners[$eventName] as $listener) {
+                $listener($event);
+            }
+        }
+    }
 }
