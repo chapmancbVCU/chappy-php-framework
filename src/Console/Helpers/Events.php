876d75551c623ed5b0ee243f69f4ebb2593d8b3f
Chad Chapman <chad.chapman2010@gmail.com>
2025-07-20 16:10:24 -0400

    #38 - Added  variable to phpdoc

--- src/Console/Helpers/Events.php
+++ src/Console/Helpers/Events.php
@@ -46,7 +46,7 @@ class '.$listenerName.' {
     /**
      * Add description for function here
      *
-     * @param '.$eventName.'
+     * @param '.$eventName.' $event
      * @return void
      */
     public function handle('.$eventName.' $event): void {
