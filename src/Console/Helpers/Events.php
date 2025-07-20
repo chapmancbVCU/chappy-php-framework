3dee6182982d029518ac63eed8b68c0cbbfd48f1
Chad Chapman <chad.chapman2010@gmail.com>
2025-07-19 20:25:37 -0400

    #38 - Implemented ability to generate new EventServiceProvider file

--- src/Console/Helpers/Events.php
+++ src/Console/Helpers/Events.php
@@ -2,9 +2,51 @@
 declare(strict_types=1);
 namespace Console\Helpers;
 
+
 /**
  * Supports operations related to Events/Listeners.
  */
 class Events {
+    protected static string $providerPath = CHAPPY_BASE_PATH.DS.'app'.DS.'providers';
 
+    public static function eventServiceProviderTemplate(string $providerName) {
+        return '<?php
+declare(strict_types=1);
+namespace App\Providers;
+
+use Core\Lib\Events\EventDispatcher;
+
+/**
+ * App namespace event service providers
+ */
+class '.$providerName.' extends ServiceProvider {
+    protected array $listen = [
+        // Add app-specific events here.
+    ];
+
+    /**
+     * Register your events with the dispatcher.
+     *
+     * @param EventDispatcher $dispatcher The dispatcher.
+     * @return void
+     */
+    public function boot(EventDispatcher $dispatcher): void {
+        foreach($this->listen as $event => $listeners) {
+            foreach($listeners as $listener) {
+                $dispatcher->listen($event, [new $listener(), \'handle\']);
+            }
+        }
+    }
+}';
+    }
+
+    public static function makeEventServiceProvider(string $providerName): int {
+        Tools::pathExists(self::$providerPath);
+        $fullPath = self::$providerPath.$providerName.'.php';
+        return Tools::writeFile(
+            $fullPath,
+            self::eventServiceProviderTemplate($providerName),
+            'Provider'
+        );
+    }
 }
