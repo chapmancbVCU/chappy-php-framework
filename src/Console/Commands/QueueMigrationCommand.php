ddddb7d39c4f87ee4d919adc7a5e385f25196b24
Chad Chapman <chad.chapman2010@gmail.com>
2025-07-22 19:50:45 -0400

    #39 - Initial commit

---
+++ src/Console/Commands/QueueMigrationCommand.php
@@ -0,0 +1,37 @@
+<?php
+namespace Console\Commands;
+
+use Console\Helpers\Notifications;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Supports ability to create new notifications migration file.
+ */
+class QueueMigrationCommand extends Command
+{
+    /**
+     * Configures the command.
+     *
+     * @return void
+     */
+    protected function configure(): void
+    {
+        $this->setName('queue:migration')
+            ->setDescription('Generates a new migration for queue table')
+            ->setHelp('php console notifications:migration');
+    }
+
+    /**
+     * Executes the command
+     *
+     * @param InputInterface $input The input.
+     * @param OutputInterface $output The output.
+     * @return int A value that indicates success, invalid, or failure.
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+       return Notifications::notificationsMigration();
+    }
+}
