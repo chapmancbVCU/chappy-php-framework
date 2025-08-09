df35d66daa211794afd006bf8f1e9b8facb06a05
Chad Chapman <chad.chapman2010@gmail.com>
2025-08-08 21:05:33 -0400

    #38, #39 - Added functions to implement interface

--- src/core/Lib/Queue/Jobs/QueuedListenerJob.php
+++ src/core/Lib/Queue/Jobs/QueuedListenerJob.php
@@ -14,4 +14,47 @@ final class QueuedListenerJob implements QueueableJobInterface {
         private int|array $backoff = 0,
         private int $maxAttempts = 0
     ) {}
+
+    public static function from(string $listenerClass, object $event, array $opts = []): self {
+
+    }
+
+    public function handle(): void {
+        $event = $this->rehydrateEvent();
+        $listener = new ($this->listenerClass)();
+        $listener->handle($event);
+    }
+
+    public function delay(): int {
+        return $this->delay;
+    }
+
+    public function backoff(): int|array {
+        return $this->backoff() ?: 0;
+    }
+
+    public function maxAttempts(): int {
+        return $this->maxAttempts ?: (int) Config::get('queue.max_attempts', 3);
+    }
+
+    public function toPayload(): array {
+        return [];
+    }
+
+    private function rehydrateEvent(): object {
+        $event = new ($this->eventClass)();
+        foreach($this->eventPayload as $k => $v) { $event->$k = $v; }
+        return $event;
+    }
+
+    public static function __set_state(array $state): self {
+        return new self(
+            $state['listenerClass'],
+            $state['eventClass'],
+            $state['eventPayload'],
+            $state['delay'] ?? 0,
+            $state['backoff'] ?? 0,
+            $state['max_attempts' ?? 0]
+        );
+    }
 }
