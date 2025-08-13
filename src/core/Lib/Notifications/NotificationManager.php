19ccdc641c311b356380c2eaa74176dcb0de61d1
Chad Chapman <chad.chapman2010@gmail.com>
2025-08-12 20:00:52 -0400

    #37 - Added phpdoc

--- src/core/Lib/Notifications/NotificationManager.php
+++ src/core/Lib/Notifications/NotificationManager.php
@@ -2,9 +2,22 @@
 declare(strict_types=1);
 namespace Core\Lib\Notifications;
 
+/**
+ * Manages the registration for notification channels.
+ */
 final class NotificationManager {
+    /**
+     * Determines if service is booted.
+     *
+     * @var bool Flag to determine if service is booted.
+     */
     protected static bool $booted = false;
 
+    /**
+     * Boots notification service on application start.
+     *
+     * @return void
+     */
     public static function boot(): void {
         if(self::$booted) return;
 
@@ -15,8 +28,10 @@ final class NotificationManager {
             $provider = new $providerClass();
 
             if(method_exists($provider, 'bootNotifications')) {
-                
+                $provider->bootNotifications();
             }
         }
+
+        self::$booted = true;
     }
 }
