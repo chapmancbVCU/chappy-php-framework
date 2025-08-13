37f54606731eecf8784c218980a1642dcb8e0e50
Chad Chapman <chad.chapman2010@gmail.com>
2025-08-12 20:01:55 -0400

    #37 - Added phpdoc

--- src/core/Lib/Providers/NotificationServiceProvider.php
+++ src/core/Lib/Providers/NotificationServiceProvider.php
@@ -4,12 +4,25 @@ namespace Core\Lib\Providers;
 
 use Core\Lib\Notifications\ChannelRegistry;
 
+/**
+ * Service provider for notifications.
+ */
 final class NotificationServiceProvider {
+    /**
+     * For future support for containers.  Bind per-channel deps here.
+     *
+     * @return void
+     */
     public function register(): void {
 
     }
 
-    public function boot(): void {
+    /**
+     * Register channels for notifications.
+     *
+     * @return void
+     */
+    public function bootNotifications(): void {
         $channels = config('notifications.channels', []);
         foreach($channels as $name => $class) {
             ChannelRegistry::register($name, $class);
