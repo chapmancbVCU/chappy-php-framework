206a76ab959c0523ee7a09c056d4d48991a61a17
Chad Chapman <chad.chapman2010@gmail.com>
2025-08-14 20:25:18 -0400

    #37 - Initial commit

---
+++ src/core/Lib/Notifications/Exceptions/ChannelException.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Core\Lib\Notifications\Exceptions;
+
+/**
+ * Handles exceptions related to invalid channels.  Extends the 
+ * NotificationException class.
+ */
+class ChannelException extends NotificationException {
+    public function __construct(
+        public readonly string $channel,
+        string $message = '',
+        ?\Throwable $previous = null
+    ) {
+        parent::__construct("[$channel] $message", 0, $previous);
+    }
+}
