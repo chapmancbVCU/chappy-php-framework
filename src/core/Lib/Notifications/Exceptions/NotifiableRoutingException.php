8edf4deeeee55541df1d782f0165cdf959ffe870
Chad Chapman <chad.chapman2010@gmail.com>
2025-08-14 20:21:52 -0400

    #37 - Initial commit

---
+++ src/core/Lib/Notifications/Exceptions/NotifiableRoutingException.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace Core\Lib\Notifications\Exceptions;
+
+/**
+ * Handles exceptions related to notification routing.  Extends the 
+ * NotificationException class.
+ */
+final class NotifiableRoutingException extends NotificationException {}
