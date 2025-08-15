1f8fab12ed2581e3496b09e0736b1aec0f6cc03d
Chad Chapman <chad.chapman2010@gmail.com>
2025-08-14 20:18:27 -0400

    #37 - Initial commit

---
+++ src/core/Lib/Notifications/Exceptions/NotificationException.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace Core\Lib\Notifications\Exceptions;
+
+/**
+ * Exception class for notifications that extends RuntimeException
+ */
+class NotificationException extends \RuntimeException {}
