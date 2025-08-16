18a83647f46c79b7b01ace63d04b99aeb1049c26
Chad Chapman <chad.chapman2010@gmail.com>
2025-08-15 21:31:14 -0400

    #37 - Initial setup of send function

--- src/core/Lib/Notifications/Channels/LogChannel.php
+++ src/core/Lib/Notifications/Channels/LogChannel.php
@@ -2,14 +2,26 @@
 declare(strict_types=1);
 namespace Core\Lib\Notifications\Channels;
 
+use Core\Lib\Logging\Logger;
 use Core\Lib\Notifications\Contracts\Channel;
 
 final class LogChannel implements Channel {
     public static function name(): string {
-        return 'name';
+        return 'log';
     }
 
     public function send(mixed $notifiable, mixed $notification, mixed $payload): void {
+        $log = [];
+
+
+        $log['message'] = method_exists($notification, 'toLog')
+            ? $notification->toLog($notifiable)
+            : (method_exists($notification, 'toArray') 
+                ? $notification->toArray($notifiable) 
+                : [(string)$notifiable]);
+
+        $log['notifiable'] = is_object($notifiable) ? get_class($notifiable) : $notifiable;
         
+        Logger::log(json_encode($log, JSON_PRETTY_PRINT), 'info');
     }
 }
