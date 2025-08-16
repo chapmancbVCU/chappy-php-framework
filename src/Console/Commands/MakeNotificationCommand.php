5706bc88ed05539baed2c018112d7360eb4e60ee
Chad Chapman <chad.chapman2010@gmail.com>
2025-08-16 15:44:42 -0400

    #37 - Initial commit

---
+++ src/Console/Commands/MakeNotificationCommand.php
@@ -0,0 +1,50 @@
+<?php
+namespace Console\Commands;
+
+use Console\Helpers\Queue;
+use Core\Lib\Utilities\Str;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Generates a new Notification class.
+ */
+class MakeNotificationCommand extends Command
+{
+    /**
+     * Configures the command.
+     *
+     * @return void
+     */
+    protected function configure(): void
+    {
+        $this->setName('make:notification')
+            ->setDescription('Generates a new notification class')
+            ->setHelp('php console make:notification <notification-name>')
+            ->addArgument(
+                'notification-name', 
+                InputArgument::REQUIRED, 
+                'Pass the name for the new notification'
+            )->addOption(
+                'channel', 
+                null, 
+                InputOption::VALUE_REQUIRED, 
+                'Comma separated list of channel names'
+            );
+    }
+
+    /**
+     * Executes the command
+     *
+     * @param InputInterface $input The input.
+     * @param OutputInterface $output The output.
+     * @return int A value that indicates success, invalid, or failure.
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        return Command::SUCCESS;
+    }
+}
