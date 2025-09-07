21778a23023b54871d63c615053defd2dd36544b
Chad Chapman <chad.chapman2010@gmail.com>
2025-09-06 22:09:49 -0400

    Added content for first 2 requirements to test

--- src/React/components/PasswordComplexityRequirements.jsx
+++ src/React/components/PasswordComplexityRequirements.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-export default function PasswordComplexityRequirements() {
+export const PasswordComplexityRequirements = () =>{
     return (
-        <>
-        </>
+        <div>
+            <h4 className="text-center">Password Requirements</h4>
+            <ul className="pl-3">
+                {import.meta.env.VITE_PW_MIN_LENGTH && 
+                    <li>Minimum {import.meta.env.VITE_PW_MIN_LENGTH} characters in length</li>
+                }
+                {import.meta.env.VITE_PW_MAX_LENGTH && 
+                    <li>Maximum {import.meta.env.VITE_PW_MAX_LENGTH} characters in length</li>
+                }
+            </ul>
+        </div>
     );
 }
