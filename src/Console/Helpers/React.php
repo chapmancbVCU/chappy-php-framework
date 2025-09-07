d7d1543e42300d84da673b5743d78c67c25678b6
Chad Chapman <chad.chapman2010@gmail.com>
2025-09-07 16:48:30 -0400

    #40 - Added content to component templates

--- src/Console/Helpers/React.php
+++ src/Console/Helpers/React.php
@@ -2,6 +2,36 @@
 declare(strict_types=1);
 namespace Chappy\Console\Helpers;
 
+use Symfony\Component\Console\Command\Command;
+
 class React {
-    
+
+    public static function defaultComponentTemplate(string $componentName): string {
+        return 'import React from "react"
+function '.$componentName.'() {
+
+    return (
+        <>
+        
+        </>
+    );
+}        
+export default '.$componentName.';';
+    }
+
+    public static function namedComponentTemplate(string $componentName): string {
+        return 'import React from "react"
+export const '.$componentName.' = () => {
+    return (
+        <>
+        
+        </>
+    );
+}        
+';
+    }
+
+    public static function makeComponent(string $componentName, bool $named): int {
+        return Command::SUCCESS;
+    }
 }
