8259bdf5eb4730af2c6438a4485b73afbb0b7bf8
Chad Chapman <chad.chapman2010@gmail.com>
2025-09-06 21:13:04 -0400

    Added jsdoc

--- src/React/utils/asset.js
+++ src/React/utils/asset.js
@@ -1,3 +1,8 @@
+/**
+ * Retrieves assets from project.
+ * @param {string} path The path to the asset.
+ * @returns The full path to the asset.
+ */
 export default function asset(path) {
     return import.meta.env.VITE_APP_DOMAIN + path;
 }
