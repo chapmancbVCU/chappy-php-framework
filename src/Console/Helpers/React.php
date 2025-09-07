b5b5507310b061bb92e1665a400df31a410d2151
Chad Chapman <chad.chapman2010@gmail.com>
2025-09-07 17:06:50 -0400

    #40 - Corrected path to components

--- src/Console/Helpers/React.php
+++ src/Console/Helpers/React.php
@@ -3,10 +3,11 @@ declare(strict_types=1);
 namespace Chappy\Console\Helpers;
 
 use Console\Helpers\Tools;
+use Core\Lib\Utilities\Str;
 use Symfony\Component\Console\Command\Command;
 
 class React {
-    public const COMPONENT_PATH = CHAPPY_BASE_PATH.DS.'resources'.DS.'components'.DS;
+    public const COMPONENT_PATH = CHAPPY_BASE_PATH.DS.'resources'.DS.'js'.DS.'components'.DS;
     public static function defaultComponentTemplate(string $componentName): string {
         return 'import React from "react"
 function '.$componentName.'() {
@@ -23,6 +24,7 @@ export default '.$componentName.';';
     public static function namedComponentTemplate(string $componentName): string {
         return 'import React from "react"
 export const '.$componentName.' = () => {
+
     return (
         <>
         
@@ -33,7 +35,7 @@ export const '.$componentName.' = () => {
     }
 
     public static function makeComponent(string $componentName, bool $named): int {
-        $componentPath = self::COMPONENT_PATH.$componentName.'.jsx';
+        $componentPath = self::COMPONENT_PATH.Str::lcfirst($componentName).'.jsx';
         $content = ($named) 
             ? self::namedComponentTemplate($componentName) 
             : self::defaultComponentTemplate($componentName);
