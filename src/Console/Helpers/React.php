9d95ad2739149f142c713d551a2cae71d8046c2c
Chad Chapman <chad.chapman2010@gmail.com>
2025-09-07 16:50:59 -0400

    #40 - Added constant for component path

--- src/Console/Helpers/React.php
+++ src/Console/Helpers/React.php
@@ -5,7 +5,7 @@ namespace Chappy\Console\Helpers;
 use Symfony\Component\Console\Command\Command;
 
 class React {
-
+    public const COMPONENT_PATH = CHAPPY_BASE_PATH.DS.'resources'.DS.'components'.DS;
     public static function defaultComponentTemplate(string $componentName): string {
         return 'import React from "react"
 function '.$componentName.'() {
