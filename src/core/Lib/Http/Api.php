e644de207acd71bd4d8e3ca06e6f2a8f22420b08
Chad Chapman <chad.chapman2010@gmail.com>
2025-09-08 21:07:51 -0400

    #40 - Implemented constructor

--- src/core/Lib/Http/Api.php
+++ src/core/Lib/Http/Api.php
@@ -13,14 +13,24 @@ class Api {
     protected int $defaultTtl;
     protected string $cacheDir;
 
+    private CONST CACHE_DIRECTORY = CHAPPY_BASE_PATH . DS . 'storage' . DS . 'cache';
+
     public function construct(
         string $baseUrl,
         array $defaultHeaders = ['Accept' => 'application/json'],
         array $defaultQuery,
         int $timeout = 6,
         int $defaultTtl = 0,
-        string $acheNamespace = 'api'
+        string $cacheNamespace = 'api'
     ) {
-        
+        $this->baseUrl = $baseUrl;
+        $this->defaultHeaders = $defaultHeaders;
+        $this->defaultQuery = $defaultQuery;
+        $this->timeout = $timeout;
+        $this->defaultTtl = $defaultTtl;
+
+        $rootCache = defined('CHAPPY_BASE_PATH') ? self::CACHE_DIRECTORY : sys_get_temp_dir();
+        $this->cacheDir = $rootCache . DS . $cacheNamespace;
+        if (!is_dir($this->cacheDir)) @mkdir($this->cacheDir, 0775, true);
     }
 }
