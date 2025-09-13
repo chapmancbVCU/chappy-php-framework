a935ad8b3988963a2fb8aa17958eb7b627ae019d
Chad Chapman <chad.chapman2010@gmail.com>
2025-09-12 21:30:04 -0400

    #40 - Invoke function to generate home/Index.jsx file

--- src/Console/Commands/ReactHomeCommand.php
+++ src/Console/Commands/ReactHomeCommand.php
@@ -31,6 +31,6 @@ class ReactHomeCommand extends Command {
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        return Command::SUCCESS;
+        return React::homeComponent();
     }
 }
