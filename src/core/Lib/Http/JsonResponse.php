53430cf9f910b486b3352c53efe4d929eab13fb1
Chad Chapman <chad.chapman2010@gmail.com>
2025-10-19 14:43:29 -0400

    Test

--- src/core/Lib/Http/JsonResponse.php
+++ src/core/Lib/Http/JsonResponse.php
@@ -7,7 +7,7 @@ use Core\Lib\Logging\Logger;
 trait JsonResponse {
 
     public function apiCsrfCheck() {
-        Logger::log($this->get('csrf_input'));
+        Logger::log($this->get('city'));
         //if(!FormHelper::checkToken($this->get('csrf_input'))) {
 
             //return $this->jsonError('Corrupted token');
