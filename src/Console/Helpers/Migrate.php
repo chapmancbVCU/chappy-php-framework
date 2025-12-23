7682fba9ca8484e927b58d1c8ca51ae6e38aec79
Chad Chapman <chad.chapman2010@gmail.com>
2025-12-23 18:29:10 -0500

    Converted migration stub to heredoc

--- src/Console/Helpers/Migrate.php
+++ src/Console/Helpers/Migrate.php
@@ -212,24 +212,25 @@ class Migrate {
      */
     public static function migrationClass(string $fileName, string $tableName): string {
         $tableName = Str::lower($tableName);
-        return '<?php
+        return <<<PHP
+<?php
 namespace Database\Migrations;
 use Core\Lib\Database\Schema;
 use Core\Lib\Database\Blueprint;
 use Core\Lib\Database\Migration;
 
 /**
- * Migration class for the '.$tableName.' table.
+ * Migration class for the {$tableName} table.
  */
-class '.$fileName.' extends Migration {
+class {$fileName} extends Migration {
     /**
      * Performs a migration for a new table.
      *
      * @return void
      */
     public function up(): void {
-        Schema::create(\''.$tableName.'\', function (Blueprint $table) {
-            $table->id();
+        Schema::create('{$tableName}', function (Blueprint \$table) {
+            \$table->id();
 
         });
     }
@@ -240,10 +241,10 @@ class '.$fileName.' extends Migration {
      * @return void
      */
     public function down(): void {
-        Schema::dropIfExists(\''.$tableName.'\');
+        Schema::dropIfExists('{$tableName}');
     }
 }
-';
+PHP;
     }
 
     /**
