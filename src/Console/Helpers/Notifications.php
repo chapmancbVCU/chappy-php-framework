1183820fc4910a0eff3c1f95451027189cbcfee6
Chad Chapman <chad.chapman2010@gmail.com>
2025-12-23 20:29:34 -0500

    Converted notifications migration stub to heredoc

--- src/Console/Helpers/Notifications.php
+++ src/Console/Helpers/Notifications.php
@@ -179,7 +179,8 @@ class Notifications {
      * @return string                    The complete PHP contents of the migration.
      */
     public static function migrationTemplate(string $fileName): string {
-        return '<?php
+        return <<<PHP
+<?php
 namespace Database\Migrations;
 use Core\Lib\Database\Schema;
 use Core\Lib\Database\Blueprint;
@@ -188,25 +189,24 @@ use Core\Lib\Database\Migration;
 /**
  * Migration class for the notifications table.
  */
-class '.$fileName.' extends Migration {
+class {$fileName} extends Migration {
     /**
      * Performs a migration for a new table.
      *
      * @return void
      */
     public function up(): void {
-        Schema::create(\'notifications\', function (Blueprint $table) {
-            $table->uuid(\'id\');
-            $table->primary(\'id\');
-            $table->string(\'type\');
-            $table->string(\'notifiable_type\');
-            $table->unsignedBigInteger(\'notifiable_id\');
-            $table->text(\'data\');
-            $table->timestamp(\'read_at\')->nullable();
-            $table->timestamps();
-            $table->index(\'created_at\');
-            $table->index(\'notifiable_type\');
-            $table->index(\'notifiable_id\');
+        Schema::create('notifications', function (Blueprint \$table) {
+            \$table->id();
+            \$table->string('type');
+            \$table->string('notifiable_type');
+            \$table->unsignedBigInteger('notifiable_id');
+            \$table->text('data');
+            \$table->timestamp('read_at')->nullable();
+            \$table->timestamps();
+            \$table->index('created_at');
+            \$table->index('notifiable_type');
+            \$table->index('notifiable_id');
         });
     }
 
@@ -216,10 +216,10 @@ class '.$fileName.' extends Migration {
      * @return void
      */
     public function down(): void {
-        Schema::dropIfExists(\'notifications\');
+        Schema::dropIfExists('notifications');
     }
 }
-';
+PHP;
     }
 
     /**
