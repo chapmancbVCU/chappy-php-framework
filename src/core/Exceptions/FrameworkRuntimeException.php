873db36d3bf1237eac3ccd4316972c078191737b
Chad Chapman <chad.chapman2010@gmail.com>
2025-12-25 17:30:11 -0500

    #42 - Initial commit

---
+++ src/core/Exceptions/FrameworkRuntimeException.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+namespace Core\Exceptions;
+
+/**
+ * Base class for this framework's exceptions that extends the 
+ * RuntimeException class.
+ */
+class FrameworkRuntimeException extends \RuntimeException {}
