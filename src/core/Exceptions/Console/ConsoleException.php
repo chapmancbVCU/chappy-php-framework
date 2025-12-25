7dfdec218ac1a095a870da343863c03ae029b68a
Chad Chapman <chad.chapman2010@gmail.com>
2025-12-25 18:54:20 -0500

    #42 - Added phpdoc

--- src/core/Exceptions/Console/ConsoleException.php
+++ src/core/Exceptions/Console/ConsoleException.php
@@ -3,4 +3,7 @@ declare(strict_types=1);
 namespace Core\Exceptions\Console;
 use Core\Exceptions\FrameworkException;
 
+/**
+ * Handles exceptions related to the console.
+ */
 final class ConsoleException extends FrameworkException {}
