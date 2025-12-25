82ec5249bef1fecb67cd51b4716c4e497cac3c9b
Chad Chapman <chad.chapman2010@gmail.com>
2025-12-25 14:48:29 -0500

    Used new function for prompt and dot notation verify

--- src/Console/Commands/MakeReactPageCommand.php
+++ src/Console/Commands/MakeReactPageCommand.php
@@ -8,9 +8,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 /**
  * Implements command for making a new view file.
@@ -39,43 +37,17 @@ class MakeReactPageCommand extends Command {
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $pageArray = explode(".", $input->getArgument('page-name'));
-        $named = $input->getOption('named');
-        if (sizeof($pageArray) !== 2) {
-            Tools::info(
-                'Issue parsing argument. Make sure your input is in the format: <directory_name>.<page_name>',
-                'debug',
-                'red'
-            );
-            return Command::FAILURE;
-        }
+        $pageArray = Tools::dotNotationVerify('page-name', $input);
+        if($pageArray == Command::FAILURE) return Command::FAILURE;
 
         $directory = React::PAGE_PATH . $pageArray[0];
+        $isDirMade = Tools::createDirWithPrompt($directory, $input, $output);
+
+        if($isDirMade == Command::FAILURE) return Command::FAILURE;
+        
         $pageName = Str::ucfirst($pageArray[1]);
         $filePath = $directory . DS . $pageName.'.jsx';
-        $helper = new QuestionHelper(); // <-- Manual instantiation to avoid `getHelper()` issues
-
-        // Debug to check if helper exists
-        if (!$helper) {
-            Tools::info('Helper could not be instantiated.', 'debug', 'red');
-            return Command::FAILURE;
-        }
-
-        // Check if directory exists
-        if (!is_dir($directory)) {
-            $question = new ConfirmationQuestion(
-                "The directory '$directory' does not exist. Do you want to create it? (y/n) ", 
-                false
-            );
-
-            if ($helper->ask($input, $output, $question)) {
-                mkdir($directory, 0755, true);
-                Tools::info("Directory created: $directory", 'blue');
-            } else {
-                Tools::info('Operation canceled.', 'debug', 'blue');
-                return Command::FAILURE;
-            }
-        }
+        $named = $input->getOption('named');
 
         return React::makePage($filePath, $pageName, $named);
     }
