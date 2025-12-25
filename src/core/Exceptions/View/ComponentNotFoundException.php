1581474f79f0fcfd504573bb505e1864562dbf5c
Chad Chapman <chad.chapman2010@gmail.com>
2025-12-25 17:15:23 -0500

    #42 - Initial commit

---
+++ src/core/Exceptions/View/ComponentNotFoundException.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+namespace Core\Exceptions;
+
+/**
+ * Class to handle events related to components not being found.
+ */
+final class ComponentNotFoundException extends ViewException {}
