5c77142e64780a2fb18354af9895bdd1cd9a29da
Chad Chapman <chad.chapman2010@gmail.com>
2025-12-25 17:13:20 -0500

    #42 - Initial commit

---
+++ src/core/Exceptions/View/ViewNotFoundException.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+namespace Core\Exceptions;
+
+/**
+ * Class to handle events related to views not being found.
+ */
+final class ViewNotFoundException extends ViewException {}
