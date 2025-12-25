f9f0694322e34ca60421bf5e5ae11857da03d066
Chad Chapman <chad.chapman2010@gmail.com>
2025-12-24 22:43:43 -0500

    Added phpdoc and static variable for file path

--- src/Console/Helpers/Validator.php
+++ src/Console/Helpers/Validator.php
@@ -3,15 +3,34 @@ declare(strict_types=1);
 namespace Console\Helpers;
 use Console\Helpers\Tools;
 
+/**
+ * Supports operations for the make:validator command.
+ */
 class Validator {
-    public static function makeValidator($validatorName) {
+    private const VALIDATOR_PATH = ROOT.DS.'app'.DS.'CustomValidators'.DS;
+    /**
+     * Venerates a new user defined validator class.
+     *
+     * @param string $validatorName Name for new validator class.
+     * @return int A value that indicates success, invalid, or failure.
+     */
+    public static function makeValidator($validatorName): int {
+        Tools::pathExists(self::VALIDATOR_PATH);
         return Tools::writeFile(
-            ROOT.DS.'app'.DS.'CustomValidators'.DS.$validatorName.'.php',
+            self::VALIDATOR_PATH.$validatorName.'.php',
             self::validatorTemplate($validatorName),
             'Custom validator class'
         );
     }
 
+    /**
+     * Returns a string containing the structure for the new custom 
+     * validator class with the name of the validator as the only parameter.
+     *
+     * @param string $validatorName The name for the new custom validator 
+     * class.
+     * @return string The contents for the new custom validator class.
+     */
     private static function validatorTemplate($validatorName): string {
         return <<<PHP
 <?php
