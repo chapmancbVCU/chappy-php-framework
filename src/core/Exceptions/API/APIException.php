4fa49ac359845ca0a99f519d775906e2567c278a
Chad Chapman <chad.chapman2010@gmail.com>
2025-12-25 17:40:28 -0500

    #42 - Initial commit

---
+++ src/core/Exceptions/API/APIException.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+namespace Core\Exceptions\API;
+use Core\Exceptions\FrameworkRuntimeException;
+
+/**
+ * Handles exceptions related to APIs.
+ */
+final class APIException extends FrameworkRuntimeException {}
