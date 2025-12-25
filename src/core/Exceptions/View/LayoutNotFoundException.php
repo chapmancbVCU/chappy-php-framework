157bad7eced4b47553c45e65f6838a235dca8be2
Chad Chapman <chad.chapman2010@gmail.com>
2025-12-25 17:08:29 -0500

    #42 - Added phpdoc

--- src/core/Exceptions/View/LayoutNotFoundException.php
+++ src/core/Exceptions/View/LayoutNotFoundException.php
@@ -2,4 +2,7 @@
 declare(strict_types=1);
 namespace Core\Exceptions;
 
+/**
+ * Class to handle events related to layouts not being found.
+ */
 final class LayoutNotFoundException extends ViewException {}
--- src/core/Exceptions/View/ViewException.php
+++ src/core/Exceptions/View/ViewException.php
@@ -2,7 +2,17 @@
 declare(strict_types=1);
 namespace Core\Exceptions;
 
+/**
+ * Handles exceptions related to views/layouts.  Extends the FrameworkException 
+ * class.
+ */
 class ViewException extends FrameworkException {
+    /**
+     * Create a new exception to handle faults related to rendering content.
+     *
+     * @param string $message Optional short message to describe what happened.
+     * @param \Throwable|null $previous Optional underlying cause for exception chaining.
+     */
     public function __construct(
         string $message = '',
         ?\Throwable $previous = null
