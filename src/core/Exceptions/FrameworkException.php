66503d3aef7c563758059c4a6a7dfcc77bed993a
Chad Chapman <chad.chapman2010@gmail.com>
2025-12-25 16:51:07 -0500

    #42 - Initial commit

---
+++ src/core/Exceptions/FrameworkException.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+namespace Core\Exceptions;
+
+/**
+ * Base class for this framework's exceptions that extends the Exception class.
+ */
+class FrameworkException extends \Exception {}
