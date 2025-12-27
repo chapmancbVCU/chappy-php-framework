d9d0cf35709804d8390450035b011e47a0ef139c
Chad Chapman <chad.chapman2010@gmail.com>
2025-12-27 10:52:06 -0500

    Initial commit

---
+++ src/core/Exceptions/Logger/LoggerLevelException.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+namespace Core\Exceptions\Logger;
+use Core\Exceptions\FrameworkRuntimeException;
+use Core\Lib\Logging\Logger;
+/**
+ * Handles exceptions related to APIs.
+ */
+final class LoggerLevelException extends FrameworkRuntimeException {
+    public function __construct(
+        public readonly string $level,
+        string $message = "",
+        ?\Throwable $previous = null
+    ) {
+        $message = $message . $level;
+        parent::__construct(
+            $message,
+            0,
+            $previous
+        );
+        Logger::log($message);
+    }
+}
