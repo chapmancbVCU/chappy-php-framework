8bca52f3ca061b0c4b9c58d872159351cfa7a03d
Chad Chapman <chad.chapman2010@gmail.com>
2025-12-27 10:57:32 -0500

    #42 - Removed commented out code

--- src/core/Exceptions/Logger/LoggerLevelException.php
+++ src/core/Exceptions/Logger/LoggerLevelException.php
@@ -4,7 +4,7 @@ namespace Core\Exceptions\Logger;
 use Core\Exceptions\FrameworkRuntimeException;
 use Core\Lib\Logging\Logger;
 /**
- * Handles exceptions related to APIs.
+ * Handles exceptions related to Logger severity levels.
  */
 final class LoggerLevelException extends FrameworkRuntimeException {
     public function __construct(
@@ -12,12 +12,12 @@ final class LoggerLevelException extends FrameworkRuntimeException {
         string $message = "",
         ?\Throwable $previous = null
     ) {
-        $message = $message . $level;
+        $message = $message . "[$level]";
         parent::__construct(
             $message,
             0,
             $previous
         );
-        Logger::log($message);
+        Logger::log($message, Logger::ERROR);
     }
 }
