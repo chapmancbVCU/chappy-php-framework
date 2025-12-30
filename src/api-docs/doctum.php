3f9e6de24f56e6120f85920ed3e0f7739c2774d2
Chad Chapman <chad.chapman2010@gmail.com>
2025-12-29 22:24:24 -0500

    Doctum works with XAMPP

--- src/api-docs/doctum.php
+++ src/api-docs/doctum.php
@@ -1,40 +1,28 @@
 <?php
+declare(strict_types=1);
 
 use Doctum\Doctum;
 use Doctum\Parser\Filter\PublicFilter;
-use Doctum\Config;
 use Symfony\Component\Finder\Finder;
 
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(__DIR__, 2));
 
-// Finder: Which files to document
 $iterator = Finder::create()
     ->files()
     ->name('*.php')
-    ->in([
-        ROOT . DS . 'src'
-    ])
+    ->in([ROOT . DS . 'src'])
     ->exclude([
-        'vendor',
-        'node_modules',
-        'config',
-        'public',
-        'logs',
-        'cache',
-        'api-docs/views'
+        'api-docs', // ignore src/api-docs (phar + generated views)
     ]);
 
-// Create Config object first (preferred in latest Doctum)
-$config = new Config($iterator, [
+return new Doctum($iterator, [
     'title' => 'Chappy.php API',
     'build_dir' => ROOT . DS . 'src' . DS . 'api-docs' . DS . 'views',
-    'cache_dir' => ROOT . DS . 'cache' . DS . 'doctum',
+    'cache_dir' => ROOT . DS . '.cache' . DS . 'doctum',
     'default_opened_level' => 2,
     'base_url' => '/api-docs/',
+
+    // IMPORTANT: Doctum 5.x expects filter as an option (no setFilter() method)
+    'filter' => new PublicFilter(),
 ]);
-
-$config->setFilter(new PublicFilter());
-
-// Return Doctum instance
-return new Doctum($config);
