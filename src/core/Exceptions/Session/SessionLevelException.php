2ef831dd3d5a2e8ce04f401b240cb2f49be2e41e
Chad Chapman <chad.chapman2010@gmail.com>
2025-12-29 19:37:35 -0500

    #42 - Initial commit

---
+++ src/core/Exceptions/Session/SessionLevelException.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+namespace Core\Exceptions\Session;
+use Core\Exceptions\FrameworkRuntimeException;
+use Core\Lib\Logging\Logger;
+/**
+ * Handles exceptions related to Logger severity levels.
+ */
+final class SessionLevelException extends FrameworkRuntimeException {
+    /**
+     * Create a new SessionLevelException.
+     *
+     * @param string $level The session level.
+     * @param string $message Optional human-readable details.
+     * @param \Throwable|null $previous Optional underlying cause for exception chaining.
+     */
+    public function __construct(
+        public readonly string $level,
+        string $message = "",
+        ?\Throwable $previous = null
+    ) {
+        $message = $message . "[$level]";
+        parent::__construct(
+            $message,
+            0,
+            $previous
+        );
+        Logger::log($message, Logger::ERROR);
+    }
+}
