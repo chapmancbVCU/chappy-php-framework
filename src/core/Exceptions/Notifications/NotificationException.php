6ad4982d08529d93d163ba4313a599e7f5cdc7d7
Chad Chapman <chad.chapman2010@gmail.com>
2026-01-01 15:45:35 -0500

    Import FrameworkRuntimeException

--- src/core/Exceptions/Notifications/NotificationException.php
+++ src/core/Exceptions/Notifications/NotificationException.php
@@ -1,9 +1,8 @@
 <?php
 declare(strict_types=1);
-
 namespace Core\Exceptions\Notifications;
-
+use Core\Exceptions\FrameworkRuntimeException;
 /**
  * Exception class for notifications that extends RuntimeException
  */
-class NotificationException extends \RuntimeException {}
+class NotificationException extends FrameworkRuntimeException {}
