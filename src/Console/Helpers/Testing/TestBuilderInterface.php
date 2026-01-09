6c49a80c1b31bdbf23543d5e386f4725ef91e30e
Chad Chapman <chad.chapman2010@gmail.com>
2026-01-08 20:40:15 -0500

    #43 - Added testIfExists to interface

--- src/Console/Helpers/Testing/TestBuilderInterface.php
+++ src/Console/Helpers/Testing/TestBuilderInterface.php
@@ -17,4 +17,13 @@ interface TestBuilderInterface {
      * @return int A value that indicates success, invalid, or failure.
      */
     public static function makeTest(string $testName, InputInterface $input): int;
+
+    /**
+     * Checks if file exists in either test suite.
+     *
+     * @param string $name The name of the file to be created.
+     * @return bool True if file exists in either test suite, otherwise we 
+     * return false.
+     */
+    public static function testIfExists(string $name): bool;
 }
