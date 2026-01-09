3a3baf5bb90ce04b02bb7fdc6eff9a4a5618c7cf
Chad Chapman <chad.chapman2010@gmail.com>
2026-01-08 19:10:05 -0500

    Implemented interface

--- src/Console/Helpers/Testing/TestBuilderInterface.php
+++ src/Console/Helpers/Testing/TestBuilderInterface.php
@@ -2,6 +2,8 @@
 declare(strict_types=1);
 namespace Console\Helpers\Testing;
 
+use Symfony\Component\Console\Input\InputInterface;
+
 interface TestBuilderInterface {
-    
+    public static function makeTest(string $testName, InputInterface $input): int;
 }
