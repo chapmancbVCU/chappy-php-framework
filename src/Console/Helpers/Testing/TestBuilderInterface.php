650085b2d05760099aea3919dca6f7d827789b3a
Chad Chapman <chad.chapman2010@gmail.com>
2026-01-08 19:14:04 -0500

    Added phpdoc

--- src/Console/Helpers/Testing/TestBuilderInterface.php
+++ src/Console/Helpers/Testing/TestBuilderInterface.php
@@ -4,6 +4,17 @@ namespace Console\Helpers\Testing;
 
 use Symfony\Component\Console\Input\InputInterface;
 
+/**
+ * Interface to be implemented by test builder classes for PHPUnit, Jest, and Vitest.
+ */
 interface TestBuilderInterface {
+    /**
+     * Creates a new test class.  When --feature flag is provided a test 
+     * feature class is created.
+     *
+     * @param string $testName The name for the test.
+     * @param InputInterface $input The Symfony InputInterface object.
+     * @return int A value that indicates success, invalid, or failure.
+     */
     public static function makeTest(string $testName, InputInterface $input): int;
 }
