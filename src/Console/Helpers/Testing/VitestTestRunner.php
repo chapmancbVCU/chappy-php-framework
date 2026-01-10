b02a370f94a10e7db96e6b10ece7cbdf9a0a9df4
Chad Chapman <chad.chapman2010@gmail.com>
2026-01-10 14:58:29 -0500

    #43 - Fixe path

--- src/Console/Helpers/Testing/VitestTestRunner.php
+++ src/Console/Helpers/Testing/VitestTestRunner.php
@@ -3,5 +3,5 @@ declare(strict_types=1);
 namespace Console\Helpers\Testing;
 
 class VitestTestRunner extends TestRunner {
-    public const UNIT_PATH = 'resources'.DS.'js'.DS.'test'.DS.'unit'.DS;
+    public const UNIT_PATH = 'resources'.DS.'js'.DS.'tests'.DS.'unit'.DS;
 }
