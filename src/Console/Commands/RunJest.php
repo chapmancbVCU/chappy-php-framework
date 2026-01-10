1b80dbf5f4628be89031543f88d8e914c33a1165
Chad Chapman <chad.chapman2010@gmail.com>
2026-01-09 19:03:14 -0500

    #43 - Initial commit

---
+++ src/Console/Commands/RunJest.php
@@ -0,0 +1,41 @@
+<?php
+namespace Console\Commands;
+
+use Console\Helpers\Testing\PHPTestBuilder;
+use Core\Lib\Utilities\Str;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * 
+ */
+class MakeTestCommand extends Command
+{
+    /**
+     * Configures the command.
+     *
+     * @return void
+     */
+    protected function configure(): void
+    {
+        $this->setName('react:jest')
+            ->setDescription('Generates a new test file!')
+            ->setHelp('php console make:test <test_name>');
+    }
+ 
+    /**
+     * Executes the command
+     *
+     * @param InputInterface $input The input.
+     * @param OutputInterface $output The output.
+     * @return int A value that indicates success, invalid, or failure.
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln(shell_exec("npm jest"));
+        return Command::SUCCESS;
+    }
+}
