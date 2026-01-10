14b14ac65362b7b615fe8d37b954d938f5847b9c
Chad Chapman <chad.chapman2010@gmail.com>
2026-01-10 17:47:13 -0500

    #43 - Finished and testing makeTest class then added phpdoc to describe class and makeTest function

--- src/Console/Helpers/Testing/VitestTestBuilder.php
+++ src/Console/Helpers/Testing/VitestTestBuilder.php
@@ -7,18 +7,48 @@ use Console\Helpers\Tools;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 
+/**
+ * Utility class that supports ability to create new tests for Vitest based 
+ * tests.
+ */
 class VitestTestBuilder implements TestBuilderInterface {
-    
+    /**
+     * Creates a new test class.  Three types of test can be generated based 
+     * on flag
+     * 
+     * 1. --component - Creates component test
+     * 2. --unit - Creates unit test
+     * 3. --view - Creates view test
+     * 
+     * @param string $testName The name for the test.
+     * @param InputInterface $input The Symfony InputInterface object.
+     * @return int A value that indicates success, invalid, or failure.
+     */
     public static function makeTest(string $testName, InputInterface $input): int { 
         if(self::testIfExists($testName)) {
             return Command::FAILURE;
         }
 
-        return Tools::writeFile(
+        if($input->getOption('component')) {
+            return Tools::writeFile(
+                ROOT.DS.VitestTestRunner::COMPONENT_PATH.$testName.'.test.js',
+                VitestStubs::unitTestStub($testName),
+                'Test'
+            );
+        } else if($input->getOption('unit')) {
+            return Tools::writeFile(
                 ROOT.DS.VitestTestRunner::UNIT_PATH.$testName.'.test.js',
                 VitestStubs::unitTestStub($testName),
                 'Test'
             );
+        } else {
+            return Tools::writeFile(
+                ROOT.DS.VitestTestRunner::VIEW_PATH.$testName.'.test.js',
+                VitestStubs::unitTestStub($testName),
+                'Test'
+            );
+        }
+        
         return Command::SUCCESS;
     }
 
