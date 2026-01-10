ce9528401905e28eac365079c8ceed72766db6c2
Chad Chapman <chad.chapman2010@gmail.com>
2026-01-10 14:41:38 -0500

    #43 - Added code to begin first write test

--- src/Console/Helpers/Testing/VitestTestBuilder.php
+++ src/Console/Helpers/Testing/VitestTestBuilder.php
@@ -2,13 +2,24 @@
 declare(strict_types=1);
 namespace Console\Helpers\Testing;
 
+use Core\Lib\Logging\Logger;
+use Console\Helpers\Tools;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 
 class VitestTestBuilder implements TestBuilderInterface {
     
     public static function makeTest(string $testName, InputInterface $input): int { 
-        return Command::FAILURE;
+        if(self::testIfExists($testName)) {
+            return Command::FAILURE;
+        }
+
+        return Tools::writeFile(
+                ROOT.DS.VitestTestRunner::UNIT_PATH.$testName.'.test.js',
+                VitestStubs::unitTestStub($testName),
+                'Test'
+            );
+        return Command::SUCCESS;
     }
 
     public static function testIfExists(string $name): bool { return false; }
