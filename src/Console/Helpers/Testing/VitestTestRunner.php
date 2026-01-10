47dfbc32a7a1d9a8b50edeb03dbfb156ed859bbf
Chad Chapman <chad.chapman2010@gmail.com>
2026-01-10 16:48:38 -0500

    #43 - Added TEST_COMMAND const, constructor, and parseOptions

--- src/Console/Helpers/Testing/VitestTestRunner.php
+++ src/Console/Helpers/Testing/VitestTestRunner.php
@@ -2,6 +2,25 @@
 declare(strict_types=1);
 namespace Console\Helpers\Testing;
 
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
 class VitestTestRunner extends TestRunner {
     public const UNIT_PATH = 'resources'.DS.'js'.DS.'tests'.DS.'unit'.DS;
+
+    public const TEST_COMMAND = "npm test ";
+    
+    /**
+     * Constructor
+     *
+     * @param InputInterface $input The Symfony InputInterface object.
+     * @param OutputInterface $output The Symfony OutputInterface object.
+     */
+    public function __construct(InputInterface $input, OutputInterface $output) {
+        $this->inputOptions = self::parseOptions($input);
+        parent::__construct($output);
+    }
+
+    public static function parseOptions(InputInterface $input): string { return ""; }
 }
