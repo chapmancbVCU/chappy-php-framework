2f1aa81d372b23ff932edac2c559bc917dbf8ebe
Chad Chapman <chad.chapman2010@gmail.com>
2026-01-10 13:50:27 -0500

    #43 - Added content to first stub function

--- src/Console/Helpers/Testing/VitestStubs.php
+++ src/Console/Helpers/Testing/VitestStubs.php
@@ -2,10 +2,18 @@
 declare(strict_types=1);
 namespace Console\Helpers\Testing;
 
+/**
+ * Collection of template stub files for vitest unit tests.
+ */
 class VitestStubs {
-    public static function vitestStub($testName): string {
+    public static function unitTestStub($testName): string {
         return <<<JS
+import { expect, test } from 'vitest'
 
+
+test('Test description ...', () => {
+
+});
 JS;
     }
 }
