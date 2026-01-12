64f5da7f31bcd1f0a094b9f1123b8d41cb5a9e85
Chad Chapman <chad.chapman2010@gmail.com>
2026-01-12 18:42:32 -0500

    #43 - Initial commit

---
+++ src/React/testing/SampleComponent.jsx
@@ -0,0 +1,7 @@
+/**
+ * Sample component for unit testing.
+ * @returns {JSX.Element} A sample component
+ */
+const SampleComponent = () => <h1>My Sample Component</h1>;
+
+export default SampleComponent;
