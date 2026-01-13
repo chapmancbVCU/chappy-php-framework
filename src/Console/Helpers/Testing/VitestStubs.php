65a02d73a4bb9c68f4932df47e5daaa3d7262fc5
Chad Chapman <chad.chapman2010@gmail.com>
2026-01-12 19:34:13 -0500

    #43 - Added stub for component/view tests and added phpdoc

--- src/Console/Helpers/Testing/VitestStubs.php
+++ src/Console/Helpers/Testing/VitestStubs.php
@@ -6,7 +6,28 @@ namespace Console\Helpers\Testing;
  * Collection of template stub files for vitest unit tests.
  */
 class VitestStubs {
-    public static function unitTestStub($testName): string {
+    /**
+     * Stub for component and view tests.
+     *
+     * @return string The test file for component and view tests.
+     */
+    public static function componentAndViewTestStub(): string {
+        return <<<JS
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+
+test('Test description ...', () => {
+
+});
+JS;
+    }
+
+    /**
+     * Stub for unit tests.
+     *
+     * @return string The test file for unit tests.
+     */
+    public static function unitTestStub(): string {
         return <<<JS
 import { expect, test } from 'vitest'
 
