e7720a158721d55ea4438451b5081052e9772f4f
Chad Chapman <chad.chapman2010@gmail.com>
2026-01-12 19:42:39 -0500

    #43 - Added phpdoc

--- src/Console/Helpers/Testing/VitestTestBuilder.php
+++ src/Console/Helpers/Testing/VitestTestBuilder.php
@@ -58,5 +58,12 @@ class VitestTestBuilder implements TestBuilderInterface {
         return Command::SUCCESS;
     }
 
+    /**
+     * Checks if file exists in any test suite.
+     *
+     * @param string $name The name of the file to be created.
+     * @return bool True if file exists in any test suite, otherwise we 
+     * return false.
+     */
     public static function testIfExists(string $name): bool { return false; }
 }
