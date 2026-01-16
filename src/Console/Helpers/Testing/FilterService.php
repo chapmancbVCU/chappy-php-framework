345bae0c0d5fcfd9c902d8578120fcd18314c014
Chad Chapman <chad.chapman2010@gmail.com>
2026-01-16 18:57:18 -0500

    #43 - Implemented class

--- src/Console/Helpers/Testing/FilterService.php
+++ src/Console/Helpers/Testing/FilterService.php
@@ -3,5 +3,24 @@ declare(strict_types=1);
 namespace Console\Helpers\Testing;
 
 class FilterService {
-    
+    private $filter;
+    private $location;
+    private $testFile;
+    public function __construct(string $filter, string $location, string $testFile) {
+        $this->filter = $filter;
+        $this->location = $location;
+        $this->testFile = $testFile;
+    }
+
+    public function filter() {
+        return $this->filter;
+    }
+
+    public function location() {
+        return $this->location;
+    }
+
+    public function testFileName() {
+        return $this->testFile;
+    }
 }
