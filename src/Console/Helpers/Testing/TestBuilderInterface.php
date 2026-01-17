b2b98b7aacabb982094a35c5f1a2993820f24829
Chad Chapman <chad.chapman2010@gmail.com>
2026-01-17 18:57:15 -0500

    #43 - Removed testIfExists from interface

--- src/Console/Helpers/Testing/TestBuilderInterface.php
+++ src/Console/Helpers/Testing/TestBuilderInterface.php
@@ -16,13 +16,4 @@ interface TestBuilderInterface {
      * @return int A value that indicates success, invalid, or failure.
      */
     public static function makeTest(string $testName, InputInterface $input): int;
-
-    /**
-     * Checks if file exists in any test suite.
-     *
-     * @param string $name The name of the file to be created.
-     * @return bool True if file exists in any test suite, otherwise we 
-     * return false.
-     */
-    public static function testIfExists(string $name): bool;
 }
