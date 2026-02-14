fad3b0768055cb5d22d4834995c59ddcf12f7d70
Chad Chapman <chad.chapman2010@gmail.com>
2026-02-13 19:42:51 -0500

    #45 - Added some functions for factory

--- src/core/Lib/Database/Factory.php
+++ src/core/Lib/Database/Factory.php
@@ -3,8 +3,46 @@
 declare(strict_types=1);
 namespace Core\Lib\Database;
 
-use Faker\Factory as Faker;
+use Faker\Factory as FakerFactory;
+use Faker\Generator;
 
 abstract class Factory {
-    
+    protected Generator $faker;
+    protected $modelName;
+    public function __construct(string $modelName) {
+        $this->faker = FakerFactory::create();
+        $this->modelName = new $modelName;
+    }
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    abstract protected function definition(): array;
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function createOne(): void {
+        $data = $this->definition();
+        $this->insert($data);
+    }
+
+    public function count(int $count): void {
+        for($i = 0; $i < $count; $i++) {
+            $this->createOne();
+        }
+    }
+
+    protected function insert(array $data): void {
+        foreach($data as $key => $value) {
+            if(property_exists($this->modelName, $key)) {
+                $this->modelName->key = $value;
+            }
+        }
+        $this->modelName->save();
+    }
 }
