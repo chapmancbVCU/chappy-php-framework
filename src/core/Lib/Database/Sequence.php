02207b4eb56d7fc521ba358560d0561468a65c1d
Chad Chapman <chad.chapman2010@gmail.com>
2026-02-21 18:36:24 -0500

    #45 - Added phpdoc

--- src/core/Lib/Database/Sequence.php
+++ src/core/Lib/Database/Sequence.php
@@ -6,8 +6,20 @@ namespace Core\Lib\Database;
  * Provides support for sequencing of database field values.
  */
 class Sequence {
-    protected $index = 0;
-    protected $sequence;
+
+    /**
+     * The index for determining sequence.
+     *
+     * @var int
+     */
+    protected int $index = 0;
+
+    /**
+     * Associative array containing data to be sequenced.
+     *
+     * @var array
+     */
+    protected array $sequence;
 
     /**
      * Constructor for the Sequence class.
@@ -22,10 +34,13 @@ class Sequence {
     /**
      * Uses modulo operator to determine sequence.
      *
+     * @param array<string, mixed> $data Data to be saved in new database record.
+     * @param array $attributes The attributes used to override default definition 
+     * values.
      * @return array An associative array of attributes that will be merged 
      * into the model's data.
      */
-    public function __invoke() {
+    public function __invoke(array $data = [], array $attributes = []) {
         $value = $this->sequence[$this->index % count($this->sequence)];
         $this->index++;
         return $value;
