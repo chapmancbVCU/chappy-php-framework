07bd6646a7eaae9b72155404bcf129916841b115
Chad Chapman <chad.chapman2010@gmail.com>
2026-03-04 18:56:14 -0500

    #46 - Added constructor and fieldName parameter to getInstance

--- src/Console/Console.php
+++ src/Console/Console.php
@@ -12,11 +12,21 @@ class Console {
     use HasValidators;
 
     /**
+     * Creates new instance of Console class.
+     *
+     * @param string $fieldName The name of the field to be validated.
+     */
+    public function __construct(string $fieldName = "") {
+        $this->fieldName($fieldName);
+    }
+
+    /**
      * Returns instance of this or child helper class.
      *
+     * @param string $fieldName The name of the field to be validated.
      * @return static
      */
-    public static function getInstance(): static {
-        return new static();
+    public static function getInstance(string $fieldName = ""): static {
+        return new static($fieldName);
     }
 }
