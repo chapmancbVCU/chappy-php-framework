c7e669bed640c6984d8ed7db429e0ba81acc2c9f
Chad Chapman <chad.chapman2010@gmail.com>
2026-03-24 19:29:28 -0400

    #46 - Removed unused imports

--- src/Console/Helpers/Testing/PHPUnitRunner.php
+++ src/Console/Helpers/Testing/PHPUnitRunner.php
@@ -2,9 +2,7 @@
 declare(strict_types=1);
 namespace Console\Helpers\Testing;
 
-use Console\Console;
 use Core\Lib\Utilities\Arr;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
