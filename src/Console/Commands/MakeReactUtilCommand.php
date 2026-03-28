8d8642298dc31142d758a2b7c7f13b68f8c08f43
Chad Chapman <chad.chapman2010@gmail.com>
2026-03-28 17:20:13 -0400

    #46 - Removed unused import

--- src/Console/Commands/MakeReactUtilCommand.php
+++ src/Console/Commands/MakeReactUtilCommand.php
@@ -5,7 +5,6 @@ use Console\Helpers\React;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
