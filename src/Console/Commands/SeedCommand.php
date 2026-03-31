ac1229a5a50a60bbeb47b6fecddd3aa13fbba984
Chad Chapman <chad.chapman2010@gmail.com>
2026-03-31 05:29:29 -0400

    #46 - Refactored to use ConsoleCommand class

--- src/Console/Commands/SeedCommand.php
+++ src/Console/Commands/SeedCommand.php
@@ -1,9 +1,7 @@
 <?php
 namespace Console\Commands;
- 
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
+
+use Console\ConsoleCommand;
 use Console\Helpers\DBSeeder;
 use Symfony\Component\Console\Input\InputOption;
 
@@ -11,7 +9,7 @@ use Symfony\Component\Console\Input\InputOption;
  * Runs the command for seeding database with random data.
  * More information can be found <a href="https://chapmancbvcu.github.io/chappy-php-starter/database_seeders#running-seeder">here</a>.
  */
-class SeedCommand extends Command {
+class SeedCommand extends ConsoleCommand {
     /**
      * Configures the command.
      *
@@ -28,12 +26,10 @@ class SeedCommand extends Command {
     /**
      * Executes the command
      *
-     * @param InputInterface $input The input.
-     * @param OutputInterface $output The output.
      * @return int A value that indicates success, invalid, or failure.
      */
-    protected function execute(InputInterface $input, OutputInterface $output): int
+    protected function handle(): int
     {
-        return DBSeeder::seed($input, $output);
+        return DBSeeder::seed($this->input, $this->question());
     }
 }
