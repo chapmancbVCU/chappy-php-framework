d1f3bf5b12ba44f1ea99c9121f033a297b3581b1
Chad Chapman <chad.chapman2010@gmail.com>
2026-03-30 20:39:05 -0400

    #46 - Refactored to use FrameworkQuestion as parameter

--- src/Console/Commands/MakeServiceCommand.php
+++ src/Console/Commands/MakeServiceCommand.php
@@ -1,18 +1,16 @@
 <?php
 namespace Console\Commands;
 
+use Console\ConsoleCommand;
 use Console\Helpers\Services;
 use Core\Lib\Utilities\Str;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Generates a new custom service by running make:service.
  * More information can be found <a href="https://chapmancbvcu.github.io/chappy-php-starter/services#user-services">here</a>.
  */
-class MakeServiceCommand extends Command {
+class MakeServiceCommand extends ConsoleCommand {
     /**
      * Configures the command.
      *
@@ -29,18 +27,16 @@ class MakeServiceCommand extends Command {
     /**
      * Executes the command
      *
-     * @param InputInterface $input The input.
-     * @param OutputInterface $output The output.
      * @return int A value that indicates success, invalid, or failure.
      */
-    protected function execute(InputInterface $input, OutputInterface $output): int
+    protected function handle(): int
     {
-        $serviceName = $input->getArgument('service-name');
+        $serviceName = $this->getArgument('service-name');
         $message = "Enter name for new service.";
         if($serviceName) {
-            Services::argOptionValidate($serviceName, $message, $input, $output, ['max:50']);
+            Services::argOptionValidate($serviceName, $message, $this->question(), ['max:50']);
         } else {
-            $serviceName = Services::prompt($message, $input, $output, ['max:50']);
+            $serviceName = Services::prompt($message, $this->question(), ['max:50']);
         }
         return Services::makeService(Str::ucfirst($serviceName));
     }
