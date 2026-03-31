f845c93f0217b9699a4c2bcefab618a2dabb85d2
Chad Chapman <chad.chapman2010@gmail.com>
2026-03-30 21:17:09 -0400

    #46 - Removed unused imports

--- src/Console/Commands/QueueWorkerCommand.php
+++ src/Console/Commands/QueueWorkerCommand.php
@@ -5,8 +5,6 @@ use Console\ConsoleCommand;
 use Console\Helpers\Queue;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Executes a queue worker.
