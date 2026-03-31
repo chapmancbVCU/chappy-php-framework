e440c996130224a1099e811feca902490550d2f4
Chad Chapman <chad.chapman2010@gmail.com>
2026-03-30 20:48:03 -0400

    #46 - Refactored to use FrameworkQuestion as parameter

--- src/Console/Commands/MakeViewCommand.php
+++ src/Console/Commands/MakeViewCommand.php
@@ -1,19 +1,17 @@
 <?php
 namespace Console\Commands;
 
+use Console\ConsoleCommand;
 use Console\Helpers\View;
 use Console\Helpers\Tools;
 use Core\Lib\Utilities\Str;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Implements command for making a new view file by running make:view.
  * More information can be found <a href="https://chapmancbvcu.github.io/chappy-php-starter/views#make-views">here</a>.
  */
-class MakeViewCommand extends Command {
+class MakeViewCommand extends ConsoleCommand {
     /**
      * Configures the command.
      *
@@ -30,33 +28,31 @@ class MakeViewCommand extends Command {
     /**
      * Executes the command
      *
-     * @param InputInterface $input The input.
-     * @param OutputInterface $output The output.
      * @return int A value that indicates success, invalid, or failure.
      */
-    protected function execute(InputInterface $input, OutputInterface $output): int
+    protected function handle(): int
     {
-        $viewName = $input->getArgument('view-name');
+        $viewName = $this->getArgument('view-name');
         $message = "Enter name for new directory and page in following format: <directory_name>.<view_name>";
         $attributes = ['max:100', 'dotNotation'];
 
         if($viewName) {
-            View::argOptionValidate($viewName, $message, $input, $output, $attributes, true);
+            View::argOptionValidate($viewName, $message, $this->question(), $attributes, true);
         } else {
-            $viewName = View::prompt($message, $input, $output, $attributes, [], null, true);
+            $viewName = View::prompt($message, $this->question(), $attributes, [], null, true);
         }
 
         // Validate directory and view.
         [$directory, $view] = explode('.', $viewName);
         $message = "Enter name for directory";
-        View::argOptionValidate($directory, $message, $input, $output, ['max:50']);
+        View::argOptionValidate($directory, $message, $this->question(), ['max:50']);
         $message = "Enter name for the new view";
-        View::argOptionValidate($view, $message, $input, $output, ['max:50']);
+        View::argOptionValidate($view, $message, $this->question(), ['max:50']);
 
         // Check if directory exists and create it.
         $directory = View::VIEW_PATH.Str::ucfirst($directory);
-        $isDirMade = Tools::createDirWithPrompt($directory, $input, $output);
-        if($isDirMade == Command::FAILURE) return Command::FAILURE;
+        $isDirMade = Tools::createDirWithPrompt($directory, $this->question());
+        if($isDirMade == self::FAILURE) return self::FAILURE;
 
         $view = Str::ucfirst($view);
         return View::makeView($directory . DS . $view.'.php');
