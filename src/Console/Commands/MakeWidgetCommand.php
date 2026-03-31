6d6b940f94e440a99a3e4377c64b9f174037e0e8
Chad Chapman <chad.chapman2010@gmail.com>
2026-03-30 20:51:57 -0400

    #46 - Refactored to use FrameworkQuestion as parameter

--- src/Console/Commands/MakeWidgetCommand.php
+++ src/Console/Commands/MakeWidgetCommand.php
@@ -1,19 +1,17 @@
 <?php
 namespace Console\Commands;
 
+use Console\ConsoleCommand;
 use Console\Helpers\View;
 use Console\Helpers\Tools;
 use Core\Lib\Utilities\Str;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Implements command for making a new widget file by running make:widget.
  * More information can be found <a href="https://chapmancbvcu.github.io/chappy-php-starter/widgets#overview">here</a>.
  */
-class MakeWidgetCommand extends Command {
+class MakeWidgetCommand extends ConsoleCommand {
     /**
      * Configures the command.
      *
@@ -30,33 +28,31 @@ class MakeWidgetCommand extends Command {
     /**
      * Executes the command
      *
-     * @param InputInterface $input The input.
-     * @param OutputInterface $output The output.
      * @return int A value that indicates success, invalid, or failure.
      */
-    protected function execute(InputInterface $input, OutputInterface $output): int
+    protected function handle(): int
     {
-        $widgetName = $input->getArgument('widget-name');
+        $widgetName = $this->getArgument('widget-name');
         $message = "Enter name for new widget in the following format: <directory_name>.<view_name>";
         $attributes = ['max:100', 'dotNotation'];
 
         if($widgetName) {
-            View::argOptionValidate($widgetName, $message, $input, $output, $attributes, true);
+            View::argOptionValidate($widgetName, $message, $this->question(), $attributes, true);
         } else {
-            $widgetName = View::prompt($message, $input, $output, $attributes, [], null, true);
+            $widgetName = View::prompt($message, $this->question(), $attributes, [], null, true);
         }
 
         // Validate directory and widget.
         [$directory, $widget] = explode('.', $widgetName);
         $message = "Enter name for directory";
-        View::argOptionValidate($directory, $message, $input, $output, ['max:50']);
+        View::argOptionValidate($directory, $message, $this->question(), ['max:50']);
         $message = "Enter name for the new widget";
-        View::argOptionValidate($widget, $message, $input, $output, ['max:50']);
+        View::argOptionValidate($widget, $message, $this->question(), ['max:50']);
 
         // Check if directory exists and create it.
         $directory = View::WIDGET_PATH.Str::ucfirst($directory);
-        $isDirMade = Tools::createDirWithPrompt($directory, $input, $output);
-        if($isDirMade == Command::FAILURE) return Command::FAILURE;
+        $isDirMade = Tools::createDirWithPrompt($directory, $this->question());
+        if($isDirMade == self::FAILURE) return self::FAILURE;
 
         $widget = Str::ucfirst($widget);
         return View::makeWidget($directory . DS . $widget.'.php');
