cabcf96f9e0bbd1c2e30e64cbff31595f69bd9f8
Chad Chapman <chad.chapman2010@gmail.com>
2026-03-30 20:14:59 -0400

    #46 - Removed unused imports

--- src/Console/Helpers/View.php
+++ src/Console/Helpers/View.php
@@ -5,8 +5,6 @@ namespace Console\Helpers;
 use Console\Console;
 use Console\FrameworkQuestion;
 use Core\Lib\Utilities\Str;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Supports ability to create views, layouts, and menu_acl 
