c2805786a908f27595631323c06e3d965efd9f2b
Chad Chapman <chad.chapman2010@gmail.com>
2026-03-30 20:58:19 -0400

    #46 - Removed unused imports

--- src/Console/Commands/MigrateFreshCommand.php
+++ src/Console/Commands/MigrateFreshCommand.php
@@ -7,8 +7,6 @@ use Console\Helpers\DBSeeder;
 use Console\Helpers\Tools;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Supports ability to drop all tables and recreate them by running migrate:fresh.
