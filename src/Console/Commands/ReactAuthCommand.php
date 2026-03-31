42714585b04f7944c3381938758e3cea784204e0
Chad Chapman <chad.chapman2010@gmail.com>
2026-03-30 21:17:52 -0400

    #46 - Refactored to use ConsoleCommand class

--- src/Console/Commands/ReactAuthCommand.php
+++ src/Console/Commands/ReactAuthCommand.php
@@ -1,16 +1,14 @@
 <?php
 namespace Console\Commands;
 
+use Console\ConsoleCommand;
 use Console\Helpers\React;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Implements command for generating the auth page components. 
  * More information can be found <a href="https://chapmancbvcu.github.io/chappy-php-starter/controllers_and_views#view-commands">here</a>.
  */
-class ReactAuthCommand extends Command {
+class ReactAuthCommand extends ConsoleCommand {
     /**
      * Configures the command.
      *
@@ -26,11 +24,9 @@ class ReactAuthCommand extends Command {
     /**
      * Executes the command
      *
-     * @param InputInterface $input The input.
-     * @param OutputInterface $output The output.
      * @return int A value that indicates success, invalid, or failure.
      */
-    protected function execute(InputInterface $input, OutputInterface $output): int
+    protected function handle(): int
     {
         return React::authComponents();
     }
