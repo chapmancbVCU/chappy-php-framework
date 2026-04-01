240ebce5e4e5ec0d26347b0e7d35bf71a209ecd9
Chad Chapman <chad.chapman2010@gmail.com>
2026-04-01 17:24:21 -0400

    #46 - Removed unused import

--- src/Console/Helpers/Model.php
+++ src/Console/Helpers/Model.php
@@ -5,7 +5,6 @@ namespace Console\Helpers;
 use Console\Console;
 use Console\FrameworkQuestion;
 use Core\Lib\Utilities\Str;
-use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
