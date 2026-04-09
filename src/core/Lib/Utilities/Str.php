668096f77a79a8091aee1b105c12e8630af95377
Chad Chapman <chad.chapman2010@gmail.com>
2026-04-09 19:39:30 -0400

    Updated phpdoc

--- src/core/Lib/Utilities/Str.php
+++ src/core/Lib/Utilities/Str.php
@@ -432,7 +432,10 @@ class Str
      *
      * @param string $haystack The string to search in.
      * @param string $needle The substring to search for.
-     * @return int|false
+     * @return int|false Returns the position where the needle exists relative 
+     * to the beginning of the haystack string (independent of search 
+     * direction or offset). Also note that string positions start at 0, and 
+     * not 1.  Returns FALSE if the needle was not found.
      */
     public static function position(string $haystack, string $needle): int|false
     {
