ab9cb8e5e47d6a9ad383583cbf79bfff61beb01b
Chad Chapman <chad.chapman2010@gmail.com>
2026-05-30 19:59:43 -0400

    Added missing error bag

--- src/views/admindashboard/edit_acl.php
+++ src/views/admindashboard/edit_acl.php
@@ -6,6 +6,7 @@
     <div class="col-md-6 bg-light p-3">
         <form class="form" action="" method="post">
             <?= csrf() ?>
+            <?= errorBag($this->displayErrors) ?>
             <?= input('text', "ACL", 'acl', $this->acl->acl, ['class' => 'form-control input-sm'], ['class' => 'form-group'], $this->displayErrors) ?>
             <div class="col-md-12 text-end pt-3">
                 <a href="<?=route('admindashboard.manageACLs')?>" class="btn btn-default">Cancel</a>
