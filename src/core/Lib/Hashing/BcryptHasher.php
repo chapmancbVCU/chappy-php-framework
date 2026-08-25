dd3114ca7b70980bf906cf81d48becf4c920b174
Chad Chapman <chad.chapman2010@gmail.com>
2026-08-24 21:02:15 -0400

    #51 - Implemented functions

--- src/core/Lib/Hashing/BcryptHasher.php
+++ src/core/Lib/Hashing/BcryptHasher.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 namespace Core\Lib\Hashing;
 
 use Core\Lib\Contracts\Hasher;
+use RuntimeException;
 
 final class BcryptHasher implements Hasher {
     private int $cost;
@@ -12,14 +13,19 @@ final class BcryptHasher implements Hasher {
     }
 
     public function make(string $value): string {
-        return "";
+        $hash = password_hash($value, PASSWORD_BCRYPT, ['cost' => $this->cost]);
+        if($hash === false) {
+            throw new RuntimeException("Bcrypt hashing failed.");
+        }
+        return $hash;
     }
 
     public function needsRehash(string $hashed): bool {
-        return true;
+        return password_needs_rehash($hashed, PASSWORD_BCRYPT, ['cost' => $this->cost]);
     }
 
     public function verify(string $value, string $hashed): bool {
-        return true;
+        if($hashed === '') return false;
+        return password_verify($value, $hashed);
     }
 }
