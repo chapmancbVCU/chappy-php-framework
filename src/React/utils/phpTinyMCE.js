c84f0ca7805f3bde3a07304e2e8dc22f324bf53c
Chad Chapman <chad.chapman2010@gmail.com>
2026-09-19 15:14:53 -0400

    #52 - Added call to addEventListener to script resulting in a simplified function call

--- src/React/utils/phpTinyMCE.js
+++ src/React/utils/phpTinyMCE.js
@@ -3,13 +3,15 @@
  * @param {string} id The id attribute for the text area field.
  */
 function initializeTinyMCE(id) {
-    tinymce.init({
-        selector: `#${id}`,
-        branding: false,
-        menubar: false,
-        height: 300,
-        plugins: 'advlist autolink lists link image charmap preview anchor searchreplace visualblocks code fullscreen insertdatetime media table code help wordcount',
-        toolbar: 'undo redo | bold italic backcolor underline strikethrough | outdent indent | alignleft aligncenter alignright alignjustify | removeformat | help',
-        license_key: 'gpl'
-    });
+    document.addEventListener("DOMContentLoaded", () => {
+        tinymce.init({
+            selector: `#${id}`,
+            branding: false,
+            menubar: false,
+            height: 300,
+            plugins: 'advlist autolink lists link image charmap preview anchor searchreplace visualblocks code fullscreen insertdatetime media table code help wordcount',
+            toolbar: 'undo redo | bold italic backcolor underline strikethrough | outdent indent | alignleft aligncenter alignright alignjustify | removeformat | help',
+            license_key: 'gpl'
+        });
+    })
 }
