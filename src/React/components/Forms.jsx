28c50321c849ecf022f800c612098d0bfc0889ba
Chad Chapman <chad.chapman2010@gmail.com>
2026-09-19 19:33:54 -0400

    #52 - Added function for color input

--- src/React/components/Forms.jsx
+++ src/React/components/Forms.jsx
@@ -176,6 +176,50 @@ export const CheckBoxRightLabel = ({
 };
 
 /**
+ * Assists in the development of input of type color in forms.  It accepts parameters 
+ * for setting  attribute tags in the form section.
+ *
+ * @typedef {Object} Color
+ * @property {string} label Sets the label for this input.
+ * @property {string}  name Sets the value for the name, for, and id attributes 
+ * for this input.
+ * @property {string} value The value we want to set.  We can use this to set 
+ * the value of the value attribute during form validation.  Default value 
+ * is the empty string.  It can be set with values during form validation 
+ * and forms used for editing records.
+ * @property {object} inputAttrs The values used to set the class and other 
+ * attributes of the input string.  The default value is an empty object.
+ * @property {object} divAttrs The values used to set the class and other 
+ * attributes of the surrounding div.  The default value is an empty object.
+ * @property {Record<string,string[]>|string[]} [errors=[]] The errors object.  
+ * Default value is an empty object.
+ *
+ * @param {Color} props
+ * @returns {HTMLDivElement} A surrounding div and the input element of type 
+ * color.
+ */
+export const Color = ({
+    label,
+    name,
+    value='',
+    inputAttrs={},
+    divAttrs={},
+    errors=[]
+}) => {
+    return (
+        <Input 
+            type="color"
+            label={label}
+            name={name}
+            value={value}
+            inputAttrs={inputAttrs}
+            divAttrs={divAttrs}
+            errors={errors}
+        />
+    )
+}
+
+/**
  * Assists in the development of input of type text in forms.  It accepts parameters 
  * for setting  attribute tags in the form section.
  *
@@ -854,6 +898,7 @@ const Forms = {
     Button,
     CheckBoxLeftLabel,
     CheckBoxRightLabel,
+    Color,
     Confirm,
     CSRFInput,
     CSRFToken, 
