290f5d01b94b189722299d36e36baf7ee220557a
Chad Chapman <chad.chapman2010@gmail.com>
2026-09-19 18:12:51 -0400

    #52 - Added global for url in forms

--- src/scripts/forms.php
+++ src/scripts/forms.php
@@ -339,7 +339,9 @@ if(!function_exists('email')) {
      * @param array $divAttrs The values used to set the class and other 
      * attributes of the surrounding div.  The default value is an empty array.
      * @param array $errors The errors array.  Default value is an empty array.
-     * @return string A surrounding div and the input element of type email.
+     * @return string The HTML div element surrounding an input of type email 
+     * with configuration and values set based on parameters entered during 
+     * function call.
      */
     function email(
         string $label, 
@@ -765,9 +767,7 @@ if(!function_exists('text')) {
      * @param array $divAttrs The values used to set the class and other 
      * attributes of the surrounding div.  The default value is an empty array.
      * @param array $errors The errors array.  Default value is an empty array.
-     * @return string The HTML div element surrounding an input of type text 
-     * with configuration and values set based on parameters entered during 
-     * function call.
+     * @return string A surrounding div and the text element.
      */
     function text(
         string $label,
@@ -806,7 +806,7 @@ if(!function_exists('textarea')) {
      * @param array $divAttrs The values used to set the class and other 
      * attributes of the surrounding div.  The default value is an empty array.
      * @param array $errors The errors array.  Default value is an empty array.
-     * @return string A surrounding div and the input element.
+     * @return string A surrounding div and the textarea element.
      */
     function textarea(
         string $label, 
@@ -827,7 +827,10 @@ if(!function_exists('textarea')) {
         );
     }
 
-    if(!function_exists('timeSelector')) {
+    
+}
+
+if(!function_exists('timeSelector')) {
     /**
      * Renders an HTML div element that surrounds an input of type timeSelector.
      *
@@ -843,9 +846,7 @@ if(!function_exists('textarea')) {
      * @param array $divAttrs The values used to set the class and other 
      * attributes of the surrounding div.  The default value is an empty array.
      * @param array $errors The errors array.  Default value is an empty array.
-     * @return string The HTML div element surrounding an input of type timeSelector 
-     * with configuration and values set based on parameters entered during 
-     * function call.
+     * @return string A surrounding div and the time element.
      */
     function timeSelector(
         string $label,
@@ -866,4 +867,44 @@ if(!function_exists('textarea')) {
         );
     }
 }
+
+if(!function_exists('url')) {
+    /**
+     * Assists in the development of url in forms.  It accepts parameters 
+     * for setting  attribute tags in the form section.
+     *
+     * @param string $label Sets the label for this input.
+     * @param string $name Sets the value for the name, for, and id attributes 
+     * for this input.
+     * @param string|null $value The value we want to set.  We can use this to set 
+     * the value of the value attribute during form validation.  Default value 
+     * is the empty string.  It can be set with values during form validation 
+     * and forms used for editing records.
+     * @param array $inputAttrs The values used to set the class and other 
+     * attributes of the input string.  The default value is an empty array.
+     * @param array $divAttrs The values used to set the class and other 
+     * attributes of the surrounding div.  The default value is an empty array.
+     * @param array $errors The errors array.  Default value is an empty array.
+     * @return string A surrounding div and the url element.
+     */
+    function url(
+        string $label, 
+        string $name, 
+        string|null $value, 
+        array $inputAttrs=[], 
+        array $divAttrs=[], 
+        array $errors=[]
+    ): string {
+        return FormHelper::inputBlock(
+            'url',
+            $label, 
+            $name, 
+            $value, 
+            $inputAttrs, 
+            $divAttrs, 
+            $errors
+        );
+    }
+
+    
 }
