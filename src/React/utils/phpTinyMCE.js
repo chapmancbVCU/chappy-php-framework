c0394e986495be1a4bb606771921a62cba63c8d7
Chad Chapman <chad.chapman2010@gmail.com>
2026-09-19 15:07:41 -0400

    #52 - Initial commit of init script for tinyMCE in php views

---
+++ src/React/utils/phpTinyMCE.js
@@ -0,0 +1,15 @@
+/**
+ * Initializes tinyMCE for PHP views.
+ * @param {string} id The id attribute for the text area field.
+ */
+function initializeTinyMCE(id) {
+    tinymce.init({
+        selector: `#${id}`,
+        branding: false,
+        menubar: false,
+        height: 300,
+        plugins: 'advlist autolink lists link image charmap preview anchor searchreplace visualblocks code fullscreen insertdatetime media table code help wordcount',
+        toolbar: 'undo redo | bold italic backcolor underline strikethrough | outdent indent | alignleft aligncenter alignright alignjustify | removeformat | help',
+        license_key: 'gpl'
+    });
+}
