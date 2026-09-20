33bb37655cd4c1994bdc3b2ceee01020f65c7209
Chad Chapman <chad.chapman2010@gmail.com>
2026-09-20 11:49:19 -0400

    #52 - Added function for time input

--- src/React/components/Forms.jsx
+++ src/React/components/Forms.jsx
@@ -1070,6 +1070,46 @@ export const TextArea = ({
     )
 }
 
+/**
+ * Renders an HTML div element that surrounds an input of type time.
+ * 
+ * @property {string} label Sets the label for this input.
+ * @property {string} name Sets the value for the name, for, and id attributes 
+ * for this input.
+ * @property {string|number} value The value we want to set.  We can use this to set 
+ * the value of the value attribute during form validation.  Default value 
+ * is the empty string.  It can be set with values during form validation 
+ * and forms used for editing records.
+ * @property {object} inputAttrs The values used to set the class and other 
+ * attributes of the input string.  The default value is an empty object.
+ * @property {object} divAttrs The values used to set the class and other 
+ * attributes of the surrounding div.  The default value is an empty object.
+ * @property {Record<string, string[]>|string[]} [errors=[]] The errors object.  
+ * Default value is an empty object.
+ * @param {InputProps} param0 
+ * @returns {HTMLDivElement} A surrounding div and the input element of type time.
+ */
+export const Time = ({
+    label,
+    name,
+    value = '',
+    inputAttrs={},     
+    divAttrs={},
+    errors=[],
+}) => {
+    return (
+        <Input 
+            type="time"
+            label={label}
+            name={name}
+            value={value}
+            inputAttrs={inputAttrs}
+            divAttrs={divAttrs}
+            errors={errors}
+        />
+    )
+}
+
 const Forms = { 
     Button,
     CheckBoxLeftLabel,
@@ -1096,6 +1136,7 @@ const Forms = {
     SubmitTag, 
     Tel,
     Text,
-    TextArea 
+    TextArea,
+    Time
 };
 export default Forms;
