3285307da5896f8ffa149ec738767bdaae5e5305
Chad Chapman <chad.chapman2010@gmail.com>
2026-09-21 20:32:58 -0400

    #52 - Added input of type date with datalist

--- src/React/components/Forms.jsx
+++ src/React/components/Forms.jsx
@@ -335,6 +335,56 @@ export const Currency = ({
 }
 
 /**
+ * Renders an HTML div element that surrounds an input of type date with an 
+ * accompanying datalist of suggestions.
+ *
+ * @typedef {Object} DataListDate
+ * @property {string} label Sets the label for this input.
+ * @property {string} name Sets the value for the name, for, and id attributes 
+ * for this input.
+ * @property {string} listName The list name name and id for the datalist element.
+ * @property {string} value The value we want to set.  We can use this to set 
+ * the value of the value attribute during form validation.  Default value 
+ * is the empty string.  It can be set with values during form validation 
+ * and forms used for editing records.
+ * @property {array} options A list of suggestions.
+ * @property {object} inputAttrs The values used to set the class and other 
+ * attributes of the input string.  The default value is an empty object.
+ * @property {object} divAttrs The values used to set the class and other 
+ * attributes of the surrounding div.  The default value is an empty object.
+ * @property {Record<string,string[]>|string[]} [errors=[]] The errors object.  
+ * Default value is an empty object.
+ *
+ * @param {DataListDate} props
+ * @returns {HTMLDivElement} A surrounding div and the input element of type 
+ * date.
+ */
+export const DataListDate = ({
+    label,
+    name,
+    listName,
+    value='',
+    options={},
+    inputAttrs={},
+    divAttrs={},
+    errors=[]
+}) => {
+    return (
+        <DataListBlock 
+            type="date"
+            label={label}
+            name={name}
+            listName={listName}
+            value={value}
+            options={options}
+            inputAttrs={inputAttrs}
+            divAttrs={divAttrs}
+            errors={errors}
+        />
+    )
+}
+
+/**
  *  Assists in the development of forms input blocks with datalist element in forms.  
  * It accepts parameters for setting attribute tags in the form section.  Not 
  * to be used for inputs of type "Submit".  For submit inputs use the submitBlock 
@@ -490,6 +540,7 @@ export const DataListTime = ({
         />
     )
 }
+
 /**
  * Renders an HTML div element that surrounds an input of type date.
  * 
@@ -1505,6 +1556,7 @@ const Forms = {
     CSRFInput,
     CSRFToken, 
     Currency,
+    DataListDate,
     DataListText,
     DataListTime,
     DateSelector,
