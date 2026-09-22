904f822040bf6c1a53e19e8bee1870c0ddcdcde2
Chad Chapman <chad.chapman2010@gmail.com>
2026-09-22 18:41:05 -0400

    #52 - Added input of type month with datalist

--- src/scripts/forms.php
+++ src/scripts/forms.php
@@ -310,6 +310,51 @@ if(!function_exists('dataListDate')) {
     }
 }
 
+if(!function_exists('dataListMonth')) {
+    /**
+     * Renders an HTML div element that surrounds an input of type month with an 
+     * accompanying datalist of suggestions.
+     *
+     * @param string $label Sets the label for this input.
+     * @param string $name Sets the value for the name, for, and id attributes 
+     * for this input.
+     * @param string $listName The list name name and id for the datalist element.
+     * @param mixed $value The value we want to set.  We can use this to set 
+     * the value of the value attribute during form validation.  Default value 
+     * is the empty string.  It can be set with values during form validation 
+     * and forms used for editing records.
+     * @param array $options A list of suggestions.
+     * @param array $inputAttrs The values used to set the class and other 
+     * attributes of the input string.  The default value is an empty array.
+     * @param array $divAttrs The values used to set the class and other 
+     * attributes of the surrounding div.  The default value is an empty array.
+     * @param array $errors The errors array.  Default value is an empty array.
+     * @return string A surrounding div and the input element of type month.
+     */
+    function dataListMonth(
+        string $label, 
+        string $name, 
+        string $listName,
+        mixed $value = '', 
+        array $options = [],
+        array $inputAttrs = [], 
+        array $divAttrs = [],
+        array $errors=[]
+    ): string {
+        return FormHelper::dataListBlock(
+            'month',
+            $label, 
+            $name, 
+            $listName,
+            $value, 
+            $options,
+            $inputAttrs, 
+            $divAttrs,
+            $errors
+        );
+    }
+}
+
 if(!function_exists('dataListText')) {
     /**
      * Renders an HTML div element that surrounds an input of type text with an 
